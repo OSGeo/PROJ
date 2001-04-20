aad23bea1a03c16ea11b57e6a83aea9062126e0e
Frank Warmerdam <warmerdam@pobox.com>
2001-04-20 12:51:12 +0000

    updated for 4.4.3 release
    git-svn-id: http://svn.osgeo.org/metacrs/proj/trunk@948 4e78687f-474d-0410-85f9-8d5e500ac6b2

--- src/pj_release.c
+++ src/pj_release.c
@@ -3,4 +3,4 @@
 static const char SCCSID[]="@(#)pj_release.c	4.5 95/09/23 GIE REL";
 #endif
 
-char const pj_release[]="Rel. 4.4.3, 12 April 2001";
+char const pj_release[]="Rel. 4.4.3, 20 April 2001";
