f227df3519b496c44b4fd48d2bf3b6395cd54163
Frank Warmerdam <warmerdam@pobox.com>
2002-12-14 20:15:30 +0000

    updated headers
    git-svn-id: http://svn.osgeo.org/metacrs/proj/trunk@1054 4e78687f-474d-0410-85f9-8d5e500ac6b2

--- proj/src/pj_open_lib.c
+++ proj/src/pj_open_lib.c
@@ -1,12 +1,48 @@
-/* standard location file open procedure */
-#ifndef lint
-static const char SCCSID[]="@(#)pj_open_lib.c	4.5   94/10/30 GIE REL";
-#endif
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  PROJ.4
+ * Purpose:  Implementation of pj_open_lib(), and pj_set_finder().  These
+ *           provide a standard interface for opening projections support
+ *           data files. 
+ * Author:   Gerald Evenden, Frank Warmerdam <warmerdam@pobox.com>
+ *
+ ******************************************************************************
+ * Copyright (c) 1995, Gerald Evenden
+ * Copyright (c) 2002, Frank Warmerdam <warmerdam@pobox.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************
+ *
+ * $Log$
+ * Revision 1.5  2002/12/14 20:15:30  warmerda
+ * updated headers
+ *
+ */
+
 #define PJ_LIB__
 #include <projects.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+
+PJ_CVSID("$Id$");
 
 static const char *(*pj_finder)(const char *) = NULL;
 static char * proj_lib_name =
