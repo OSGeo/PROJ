6d68471bcc547f2746da919d7c40eb94e5751b90
Frank Warmerdam <warmerdam@pobox.com>
2005-07-05 16:31:48 +0000

    Added standard headers.
    git-svn-id: http://svn.osgeo.org/metacrs/proj/trunk@1295 4e78687f-474d-0410-85f9-8d5e500ac6b2

--- src/jniproj.c
+++ src/jniproj.c
@@ -1,3 +1,38 @@
+/******************************************************************************
+ * $Id$
+ *
+ * Project:  PROJ.4
+ * Purpose:  Java/JNI wrappers for PROJ.4 API.
+ * Author:   Antonello Andrea
+ *
+ ******************************************************************************
+ * Copyright (c) 2005, Antonello Andrea
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ******************************************************************************
+ *
+ * $Log$
+ * Revision 1.3  2005/07/05 16:31:48  fwarmerdam
+ * Added standard headers.
+ *
+ */
+
 /*!
 * \file jniproj.c
 *
@@ -5,11 +40,10 @@
 * functions used by the java/jni wrappers of jproj4
 *
 *
-* $Id$
-*
 * \author Antonello Andrea
 * \date   Wed Oct 20 23:10:24 CEST 2004
 */
+
 #include "proj_config.h"
 
 #ifdef JNI_ENABLED
@@ -19,6 +53,8 @@
 #include <jni.h>
 
 #define arraysize 300
+
+PJ_CVSID("$Id$");
 
 /*!
  * \brief
