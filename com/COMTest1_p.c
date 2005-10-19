12da8931c0fc99cb205aaae6d6a5b0b426825c91
Frank Warmerdam <warmerdam@pobox.com>
2005-10-19 03:21:41 +0000

    updated
    git-svn-id: http://svn.osgeo.org/metacrs/proj/trunk@1309 4e78687f-474d-0410-85f9-8d5e500ac6b2

--- com/COMTest1_p.c
+++ com/COMTest1_p.c
@@ -2,9 +2,9 @@
 
 
 /* File created by MIDL compiler version 5.01.0164 */
-/* at Tue Oct 18 23:10:12 2005
+/* at Tue Oct 18 23:21:00 2005
  */
-/* Compiler settings for C:\warmerda\COMTest1\COMTest1.idl:
+/* Compiler settings for C:\warmerda\proj\com\COMTest1.idl:
     Oicf (OptLev=i2), W1, Zp8, env=Win32, ms_ext, c_ext
     error checks: allocation ref bounds_check enum stub_data 
 */
