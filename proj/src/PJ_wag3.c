c3e9920eff1f3330bc94bbaa33660a1646fa272f
Andrey Kiselev <dron@ak4719.spb.edu>
2007-12-21 14:22:27 +0000

    Added missed "lat_ts" parameter to projection description string.
    git-svn-id: http://svn.osgeo.org/metacrs/proj/trunk@1422 4e78687f-474d-0410-85f9-8d5e500ac6b2

--- proj/src/PJ_wag3.c
+++ proj/src/PJ_wag3.c
@@ -5,7 +5,7 @@ static const char SCCSID[]="@(#)PJ_wag3.c	4.1	94/02/15	GIE	REL";
 	double	C_x;
 #define PJ_LIB__
 # include	<projects.h>
-PROJ_HEAD(wag3, "Wagner III") "\n\tPCyl., Sph.";
+PROJ_HEAD(wag3, "Wagner III") "\n\tPCyl., Sph.\n\tlat_ts=";
 #define TWOTHIRD 0.6666666666666666666667
 FORWARD(s_forward); /* spheroid */
 	xy.x = P->C_x * lp.lam * cos(TWOTHIRD * lp.phi);
