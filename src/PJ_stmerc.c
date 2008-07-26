be6cf1ebc8c18bb3ab9b6ae827092d6e5acb908d
Didier Richard <didier.richard@ign.fr>
2008-07-26 10:38:45 +0000

    Gauss-Laborde Projection changed into Simplified Transverse Mercator (Schreiber, Gauss-Laborde).
    git-svn-id: http://svn.osgeo.org/metacrs/proj/trunk@1463 4e78687f-474d-0410-85f9-8d5e500ac6b2

---
+++ src/PJ_stmerc.c
@@ -0,0 +1,47 @@
+#ifndef lint
+static const char SCCSID[]="@(#)PJ_stmerc.c	4.5	07/09/24	???	???";
+#endif
+#define PROJ_PARMS__ \
+	double lamc;\
+	double phic;\
+	double c;\
+	double n1;\
+	double n2;\
+    double XS;\
+    double YS;
+
+#define PJ_LIB__
+# include	<projects.h>
+PROJ_HEAD(stmerc, "Simplified Transverse Mercator (Schreiber, Gauss-Laborde)")
+	"\n\tCyl, Sph&Ell\n\tlat_0= lon_0= k_0=";
+FORWARD(s_forward); /* spheroid */
+	double L, Ls, sinLs1, Ls1;
+	L= P->n1*lp.lam;
+    Ls= P->c+P->n1*log(pj_tsfn(-1.0*lp.phi,-1.0*sin(lp.phi),P->e));
+    sinLs1= sin(L)/cosh(Ls);
+    Ls1= log(pj_tsfn(-1.0*asin(sinLs1),0.0,0.0));
+    xy.x= (P->XS + P->n2*Ls1)*P->ra;
+    xy.y= (P->YS + P->n2*atan(sinh(Ls)/cos(L)))*P->ra;
+	return (xy);
+}
+INVERSE(s_inverse); /* spheroid */
+	double L, LC, sinC;
+	L= atan(sinh((xy.x - P->XS)*P->a/P->n2)/cos((xy.y - P->YS)*P->a/P->n2));
+    sinC= sin((xy.y - P->YS)*P->a/P->n2)/cosh((xy.x - P->XS)*P->a/P->n2);
+    LC= log(pj_tsfn(-1.0*asin(sinC),0.0,0.0));
+    lp.lam= L/P->n1;
+    lp.phi= -1.0*pj_phi2(exp((LC-P->c)/P->n1),P->e);
+	return (lp);
+}
+FREEUP; if (P) pj_dalloc(P); }
+ENTRY0(stmerc)
+    P->n1= sqrt(1.0+P->es*pow(cos(P->phi0),4.0)/(1.0-P->es));
+    P->phic= asin(sin(P->phi0)/P->n1);
+    P->c=       log(pj_tsfn(-1.0*P->phic,0.0,0.0))
+         -P->n1*log(pj_tsfn(-1.0*P->phi0,-1.0*sin(P->phi0),P->e));
+    P->n2= P->k0*P->a*sqrt(1.0-P->es)/(1.0-P->es*sin(P->phi0)*sin(P->phi0));
+    P->XS= 0;
+    P->YS= -1.0*P->n2*P->phic;
+	P->inv= s_inverse;
+	P->fwd= s_forward;
+ENDENTRY(P)
