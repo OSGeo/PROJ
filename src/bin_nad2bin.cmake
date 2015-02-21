8f0fe234ac49dc96ab0c9066d6dd730888b7a015
Even Rouault <even.rouault@mines-paris.org>
2015-02-21 14:08:23 +0000

    src/bin_nad2bin.cmake: backward test for nad2nad warning. bad directory specified for emess (from Charles Karney)
    git-svn-id: http://svn.osgeo.org/metacrs/proj/trunk@2621 4e78687f-474d-0410-85f9-8d5e500ac6b2

--- src/bin_nad2bin.cmake
+++ src/bin_nad2bin.cmake
@@ -1,12 +1,12 @@
-if(WIN32 AND NOT BUILD_LIBPROJ_SHARED)
+if(WIN32 AND BUILD_LIBPROJ_SHARED)
     message(warning " nad2nad can't be build with a DLL proj4 library you need a static lib")
-endif(WIN32 AND NOT BUILD_LIBPROJ_SHARED)
+endif(WIN32 AND BUILD_LIBPROJ_SHARED)
 
 
 set(NAD2BIN_SRC nad2bin.c)
 source_group("Source Files\\Bin" FILES ${NAD2BIN_SRC})
 if(WIN32)
-    set(NAD2BIN_SRC ${NAD2BIN_SRC} ${NAD2BIN_SRC}/emess.c)
+    set(NAD2BIN_SRC ${NAD2BIN_SRC} emess.c)
 endif(WIN32)
     
 #Executable
