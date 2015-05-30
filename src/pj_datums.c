3a84f039c4e7e91c8aecec790b948cb9b4c7f955
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2015-05-29 23:53:59 -0400

    Fix warnings about structure initializers.

--- src/pj_datums.c
+++ src/pj_datums.c
@@ -40,21 +40,30 @@
 C_NAMESPACE_VAR struct PJ_DATUMS pj_datums[] = {
 /* id       definition                               ellipse  comments */
 /* --       ----------                               -------  -------- */
-"WGS84",    "towgs84=0,0,0", 		             "WGS84", "",
-"GGRS87",   "towgs84=-199.87,74.79,246.62",          "GRS80", 
-				"Greek_Geodetic_Reference_System_1987",
-"NAD83",    "towgs84=0,0,0",                         "GRS80", 
-				"North_American_Datum_1983",
-"NAD27",    "nadgrids=@conus,@alaska,@ntv2_0.gsb,@ntv1_can.dat",           
-                                                     "clrk66", 
-				"North_American_Datum_1927",
-"potsdam",  "towgs84=598.1,73.7,418.2,0.202,0.045,-2.455,6.7",  "bessel",  "Potsdam Rauenberg 1950 DHDN",
-"carthage",  "towgs84=-263.0,6.0,431.0",  "clrk80ign",  "Carthage 1934 Tunisia",
-"hermannskogel", "towgs84=577.326,90.129,463.919,5.137,1.474,5.297,2.4232",  "bessel",  "Hermannskogel",
-"ire65",  "towgs84=482.530,-130.596,564.557,-1.042,-0.214,-0.631,8.15",  "mod_airy",  "Ireland 1965",
-"nzgd49",    "towgs84=59.47,-5.04,187.44,0.47,-0.1,1.024,-4.5993", "intl", "New Zealand Geodetic Datum 1949",
-"OSGB36",    "towgs84=446.448,-125.157,542.060,0.1502,0.2470,0.8421,-20.4894", "airy", "Airy 1830",
-NULL,       NULL,                                    NULL,    NULL 
+{"WGS84",   "towgs84=0,0,0",                         "WGS84", ""},
+{"GGRS87",  "towgs84=-199.87,74.79,246.62",          "GRS80",
+                                                              "Greek_Geodetic_Reference_System_1987"},
+{"NAD83",   "towgs84=0,0,0",                         "GRS80",
+                                                              "North_American_Datum_1983"},
+{"NAD27",   "nadgrids=@conus,@alaska,@ntv2_0.gsb,@ntv1_can.dat",
+                                                     "clrk66",
+                                                              "North_American_Datum_1927"},
+{"potsdam", "towgs84=598.1,73.7,418.2,0.202,0.045,-2.455,6.7",
+                                                     "bessel",
+                                                              "Potsdam Rauenberg 1950 DHDN"},
+{"carthage","towgs84=-263.0,6.0,431.0",              "clrk80ign",
+                                                              "Carthage 1934 Tunisia"},
+{"hermannskogel",  "towgs84=577.326,90.129,463.919,5.137,1.474,5.297,2.4232",
+                                                     "bessel",
+                                                              "Hermannskogel"},
+{"ire65",   "towgs84=482.530,-130.596,564.557,-1.042,-0.214,-0.631,8.15",
+                                                     "mod_airy",
+                                                              "Ireland 1965"},
+{"nzgd49",  "towgs84=59.47,-5.04,187.44,0.47,-0.1,1.024,-4.5993",
+                                                     "intl",  "New Zealand Geodetic Datum 1949"},
+{"OSGB36",  "towgs84=446.448,-125.157,542.060,0.1502,0.2470,0.8421,-20.4894",
+                                                     "airy",  "Airy 1830"},
+{NULL,      NULL,                                    NULL,    NULL}
 };
 
 struct PJ_DATUMS *pj_get_datums_ref()
@@ -66,20 +75,20 @@ struct PJ_DATUMS *pj_get_datums_ref()
 C_NAMESPACE_VAR struct PJ_PRIME_MERIDIANS pj_prime_meridians[] = {
     /* id        definition                         */
     /* --        ----------                         */
-    "greenwich", "0dE",
-    "lisbon",    "9d07'54.862\"W",
-    "paris",     "2d20'14.025\"E",
-    "bogota",    "74d04'51.3\"W",
-    "madrid",    "3d41'16.58\"W",
-    "rome",      "12d27'8.4\"E",
-    "bern",      "7d26'22.5\"E",
-    "jakarta",   "106d48'27.79\"E",
-    "ferro",     "17d40'W",
-    "brussels",  "4d22'4.71\"E",
-    "stockholm", "18d3'29.8\"E",
-    "athens",    "23d42'58.815\"E",
-    "oslo",      "10d43'22.5\"E",
-    NULL,        NULL 
+    {"greenwich", "0dE"},
+    {"lisbon",    "9d07'54.862\"W"},
+    {"paris",     "2d20'14.025\"E"},
+    {"bogota",    "74d04'51.3\"W"},
+    {"madrid",    "3d41'16.58\"W"},
+    {"rome",      "12d27'8.4\"E"},
+    {"bern",      "7d26'22.5\"E"},
+    {"jakarta",   "106d48'27.79\"E"},
+    {"ferro",     "17d40'W"},
+    {"brussels",  "4d22'4.71\"E"},
+    {"stockholm", "18d3'29.8\"E"},
+    {"athens",    "23d42'58.815\"E"},
+    {"oslo",      "10d43'22.5\"E"},
+    {NULL,        NULL}
 };
 
 struct PJ_PRIME_MERIDIANS *pj_get_prime_meridians_ref()
