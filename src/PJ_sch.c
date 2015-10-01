8b39a507a9c2e899b67e8d87c3ab0632cf1f5a38
Piyush Agram <piyush.agram@jpl.nasa.gov>
2015-10-01 11:17:25 -0700

    temporarily reverting. Please dont merge yet

--- src/PJ_sch.c
+++ src/PJ_sch.c
@@ -11,7 +11,7 @@
  *         data based on the GeoSAR processor." Airsar earth science and applications
  *         workshop, March. 2002. (http://airsar.jpl.nasa.gov/documents/workshop2002/papers/T3.pdf)
  *
- * Author:   Piyush Agram (piyush.shanker@gmail.com)
+ * Author:   Piyush Agram (piyush.agram@jpl.nasa.gov)
  *
  * NOTE:  The SCH coordinate system is a sensor aligned coordinate system 
  * developed at JPL for radar mapping missions. Details pertaining to the 
@@ -49,37 +49,9 @@
 PROJ_HEAD(sch, "Spherical Cross-track Height") "\n\tMisc\n\tplat_0 = ,plon_0 = , phdg_0 = ,[h_0 = ]";
 
 INVERSE3D(inverse3d);
-        double temp[3];
-        double pxyz[3];
-
-        //Local lat,lon using radius
-        pxyz[0] = xyz.y * P->a / P->rcurv;
-        pxyz[1] = xyz.x * P->a / P->rcurv;
-        pxyz[2] = xyz.z;
-
-
-        if( pj_Convert_Geodetic_To_Geocentric( &(P->sph), pxyz[0], pxyz[1], pxyz[2],
-                    temp, temp+1, temp+2) != 0)
-                I3_ERROR;
-
-        //Apply rotation
-        pxyz[0] = P->transMat[0] * temp[0] + P->transMat[1] * temp[1] + P->transMat[2] * temp[2];
-        pxyz[1] = P->transMat[3] * temp[0] + P->transMat[4] * temp[1] + P->transMat[5] * temp[2];
-        pxyz[2] = P->transMat[6] * temp[0] + P->transMat[7] * temp[1] + P->transMat[8] * temp[2];
-
-        //Apply offset
-        pxyz[0] += P->xyzoff[0];
-        pxyz[1] += P->xyzoff[1];
-        pxyz[2] += P->xyzoff[2];
-
-        //Convert geocentric coordinates to lat lon
-        pj_Convert_Geocentric_To_Geodetic( &(P->elp_0), pxyz[0], pxyz[1], pxyz[2],
-                temp, temp+1, temp+2);
-
-
-        lpz.lam = temp[0] ;
-        lpz.phi = temp[1] ;
-        lpz.z = temp[2];
+        lpz.lam = xyz.x ;
+        lpz.phi = xyz.y ;
+        lpz.z = xyz.z;
 
 //        printf("INVERSE: \n");
 //        printf("XYZ: %f %f %f \n", xyz.x, xyz.y, xyz.z);
@@ -88,33 +60,11 @@ INVERSE3D(inverse3d);
 }
 
 FORWARD3D(forward3d);
-        double temp[3];
-        double pxyz[3];
-
-        //Convert lat lon to geocentric coordinates
-        if( pj_Convert_Geodetic_To_Geocentric( &(P->elp_0), lpz.lam, lpz.phi, lpz.z,
-                    temp, temp+1, temp+2 ) != 0 )
-            F3_ERROR;
-
-        //Adjust for offset
-        temp[0] -= P->xyzoff[0];
-        temp[1] -= P->xyzoff[1];
-        temp[2] -= P->xyzoff[2];
-
-       
-        //Apply rotation 
-        pxyz[0] = P->transMat[0] * temp[0] + P->transMat[3] * temp[1] + P->transMat[6] * temp[2];
-        pxyz[1] = P->transMat[1] * temp[0] + P->transMat[4] * temp[1] + P->transMat[7] * temp[2];
-        pxyz[2] = P->transMat[2] * temp[0] + P->transMat[5] * temp[1] + P->transMat[8] * temp[2];
-
-        //Convert to local lat,lon
-        pj_Convert_Geocentric_To_Geodetic( &(P->sph), pxyz[0], pxyz[1], pxyz[2],
-                temp, temp+1, temp+2);
 
         //Scale by radius
-        xyz.x = temp[1] * P->rcurv / P->a;
-        xyz.y = temp[0] * P->rcurv / P->a;
-        xyz.z = temp[2];
+        xyz.x = lpz.lam;
+        xyz.y = lpz.phi;
+        xyz.z = lpz.z;
 
 //        printf("FORWARD: \n");
 //        printf("LPZ: %f %f %f \n", lpz.lam, lpz.phi, lpz.z);
@@ -126,65 +76,6 @@ FORWARD3D(forward3d);
 FREEUP; if (P) pj_dalloc(P); }
 	static PJ *
 setup(PJ *P) { /* general initialization */
-        double reast, rnorth;
-        double chdg, shdg;
-        double clt, slt;
-        double clo, slo;
-        double temp;
-        double pxyz[3];
-
-        temp = P->a * sqrt(1.0 - P->es);
-
-        //Setup original geocentric system
-        if ( pj_Set_Geocentric_Parameters(&(P->elp_0), P->a, temp) != 0)
-                E_ERROR(-37);
-
-
-        clt = cos(P->plat);
-        slt = sin(P->plat);
-        clo = cos(P->plon);
-        slo = sin(P->plon);
-
-        //Estimate the radius of curvature for given peg
-        temp = sqrt(1.0 - (P->es) * slt * slt);
-        reast = (P->a)/temp;
-        rnorth = (P->a) * (1.0 - (P->es))/pow(temp,3);
-
-        chdg = cos(P->phdg);
-        shdg = sin(P->phdg);
-        
-        P->rcurv = P->h0 + (reast*rnorth)/(reast * chdg * chdg + rnorth * shdg * shdg);
-
-//        printf("North Radius: %f \n", rnorth);
-//        printf("East Radius: %f \n", reast);
-//        printf("Effective Radius: %f \n", P->rcurv);
-
-        //Set up local sphere at the given peg point
-        if ( pj_Set_Geocentric_Parameters(&(P->sph), P->rcurv, P->rcurv) != 0)
-            E_ERROR(-37);
-
-        //Set up the transformation matrices
-        P->transMat[0] = clt * clo; 
-        P->transMat[1] = -shdg*slo - slt*clo * chdg;
-        P->transMat[2] =  slo*chdg - slt*clo*shdg;
-        P->transMat[3] =  clt*slo;
-        P->transMat[4] =  clo*shdg - slt*slo*chdg;
-        P->transMat[5] = -clo*chdg - slt*slo*shdg;
-        P->transMat[6] =  slt;
-        P->transMat[7] =  clt*chdg;
-        P->transMat[8] =  clt*shdg;        
-
-        
-        if( pj_Convert_Geodetic_To_Geocentric( &(P->elp_0), P->plat, P->plon, P->h0, 
-                                               pxyz, pxyz+1, pxyz+2 ) != 0 )
-        {
-            E_ERROR(-14)
-        }
-
-
-        P->xyzoff[0] = pxyz[0] - (P->rcurv) * clt * clo;
-        P->xyzoff[1] = pxyz[1] - (P->rcurv) * clt * slo;
-        P->xyzoff[2] = pxyz[2] - (P->rcurv) * slt;
 
 //        printf("Offset: %f %f %f \n", P->xyzoff[0], P->xyzoff[1], P->xyzoff[2]);
 
