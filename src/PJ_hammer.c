3d2501d0ee7b4724677cb19c6968888c4da31c3c
Jeff Whitaker <jswhit@fastmail.fm>
2015-12-13 07:40:20 -0700

    changed spaces to tabs (to match rest of code)

--- src/PJ_hammer.c
+++ src/PJ_hammer.c
@@ -15,16 +15,16 @@ FORWARD(s_forward); /* spheroid */
 	return (xy);
 }
 INVERSE(s_inverse); /* spheroid */
-        double z;
+	double z;
 	z = sqrt(1. - 0.25*P->w*P->w*xy.x*xy.x - 0.25*xy.y*xy.y);
 	if (fabs(2.*z*z-1.) < EPS) {
-           lp.lam = HUGE_VAL;
-           lp.phi = HUGE_VAL;
-           pj_errno = -14;
+		lp.lam = HUGE_VAL;
+		lp.phi = HUGE_VAL;
+		pj_errno = -14;
 	} else {
-	   lp.lam = aatan2(P->w * xy.x * z,2. * z * z - 1)/P->w;
-	   lp.phi = aasin(P->ctx,z * xy.y);
-        }
+		lp.lam = aatan2(P->w * xy.x * z,2. * z * z - 1)/P->w;
+		p.phi = aasin(P->ctx,z * xy.y);
+	}
 	return (lp);
 }
 FREEUP; if (P) pj_dalloc(P); }
