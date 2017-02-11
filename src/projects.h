0ad4e2af7b3e188f71e77ca71a1aacc1fa1a7647
Etienne Jacques <etienne.rtp@gmail.com>
2017-02-10 22:26:54 -0800

    Consistent precision for mathematical macros

--- src/projects.h
+++ src/projects.h
@@ -102,9 +102,9 @@ extern double hypot(double, double);
  * the -ansi flag.
  */
 #ifndef M_PI
-#define M_PI            3.14159265358979310
-#define M_PI_2          1.57079632679489660
-#define M_PI_4          0.78539816339744828
+#define M_PI            3.14159265358979323846
+#define M_PI_2          1.57079632679489661923
+#define M_PI_4          0.78539816339744830962
 #define M_2_PI          0.63661977236758134308
 #endif
 
