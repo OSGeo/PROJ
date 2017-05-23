a54611f16cf2140d93f8a33563bbc79af136636a
Kristian Evers <kristianevers@gmail.com>
2017-05-23 17:37:42 +0200

    Fix possible zero division.
    For some extreme values of eccentricity a zero divison can occur. In
    those rare cases we return HUGE_VAL to indicate something went wrong
    while still returning a defined value.
    
    Credit to OSS Fuzz.
    
    https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=1801

--- src/pj_tsfn.c
+++ src/pj_tsfn.c
@@ -2,9 +2,15 @@
 #include <math.h>
 #include <projects.h>
 
-	double
-pj_tsfn(double phi, double sinphi, double e) {
-	sinphi *= e;
-	return (tan (.5 * (M_HALFPI - phi)) /
-	   pow((1. - sinphi) / (1. + sinphi), .5 * e));
+double pj_tsfn(double phi, double sinphi, double e) {
+    double denominator;
+    sinphi *= e;
+
+    /* avoid zero division, fail gracefully */
+    denominator = 1.0 + sinphi;
+    if (denominator == 0.0)
+        return HUGE_VAL;
+
+    return (tan (.5 * (M_HALFPI - phi)) /
+            pow((1. - sinphi) / (denominator), .5 * e));
 }
