231df5a72fcd5d37c21e759f349362574c9613ef
Even Rouault <even.rouault@spatialys.com>
2017-08-20 00:35:37 +0200

    PJ_aea.c: fix memory leak in error code path. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=2723. Credit to OSS Fuzz

--- src/PJ_aea.c
+++ src/PJ_aea.c
@@ -175,7 +175,9 @@ static PJ *setup(PJ *P) {
             m2 = pj_msfn(sinphi, cosphi, P->es);
             ml2 = pj_qsfn(sinphi, P->e, P->one_es);
             if (ml2 == ml1)
-                return NULL;
+            {
+                return freeup_new(P);
+            }
             Q->n = (m1 * m1 - m2 * m2) / (ml2 - ml1);
         }
         Q->ec = 1. - .5 * P->one_es * log((1. - P->e) /
