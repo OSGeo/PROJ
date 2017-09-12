261c7497c793a8bad19bebf114294f8b54c47d68
Kristian Evers <kristianevers@gmail.com>
2017-09-12 11:22:53 +0200

    Move python version setup to build matrix. Different Travis platforms seem to have different setups. This *should* take care of the differing python setups between linux and osx.

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-pyenv global system 3.6
+# Specify python version. This introduced because the
+# Travis setup seems to vary between platforms.
+pyenv global system $PY_VERSION
+pyenv versions # a bit of debug info
+
 pip install --user cpp-coveralls
 ./travis/docker.sh
 
