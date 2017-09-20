f614731e180af45151820dadac7f1c686d3c0823
Kristian Evers <kristianevers@gmail.com>
2017-09-20 15:02:42 +0200

    Use python 3.5 with pyenv

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -6,5 +6,5 @@
 # Specify which version of python to use. The default python to use on Travis
 # can vary from platform to platform, so we use pyenv to make sure we use
 # a version that works for the complete build processs.
-pyenv global system 3.4
+pyenv global system 3.5
 pyenv versions # a bit of debug info
