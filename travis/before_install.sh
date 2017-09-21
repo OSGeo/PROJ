9685ab3562020d0df2dd61612d0dc44cd2ea0531
Kristian Evers <kristianevers@gmail.com>
2017-09-21 11:23:27 +0200

    Use only python2

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -6,17 +6,17 @@
 # Specify which version of python to use. The default python to use on Travis
 # can vary from platform to platform, so we use pyenv to make sure we use
 # a version that works for the complete build processs.
-pyenv global system 3.5
-pyenv versions # a bit of debug info
+#pyenv global system 3.5
+#pyenv versions # a bit of debug info
 
 which python
 python --version
 which python3
 python3 --version
-which python35
-python3.5 --version
+#which python35
+#python3.5 --version
 
-ls /opt/pyenv/shims
+#ls /opt/pyenv/shims
 
 which pip
 pip --version
