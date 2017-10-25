47dd489e1def98f583a3288e4e3f101ae741b4e2
Thomas Knudsen <thokn@sdfe.dk>
2017-10-25 20:46:56 +0200

    Remove superfluous internal selftests from projection files matching patterns PJ_f....c and PJ_e....c

--- src/PJ_gins8.c
+++ src/PJ_gins8.c
@@ -31,32 +31,4 @@ PJ *PROJECTION(gins8) {
 }
 
 
-#ifndef PJ_SELFTEST
-int pj_gins8_selftest (void) {return 0;}
-#else
-
-int pj_gins8_selftest (void) {
-    double tolerance_lp = 1e-10;
-    double tolerance_xy = 1e-7;
-
-    char s_args[] = {"+proj=gins8   +a=6400000    +lat_1=0.5 +lat_2=2"};
-
-    LP fwd_in[] = {
-        { 2, 1},
-        { 2,-1},
-        {-2, 1},
-        {-2,-1}
-    };
-
-    XY s_fwd_expect[] = {
-        { 194350.25093959007,  111703.90763533533},
-        { 194350.25093959007, -111703.90763533533},
-        {-194350.25093959007,  111703.90763533533},
-        {-194350.25093959007, -111703.90763533533},
-    };
-
-    return pj_generic_selftest (0, s_args, tolerance_xy, tolerance_lp, 4, 4, fwd_in, 0, s_fwd_expect, 0, 0, 0);
-}
-
-
-#endif
+int pj_gins8_selftest (void) {return 10000;}
