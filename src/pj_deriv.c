f85a9736c9afbf50f0ebe24d787234104271fd9e
Thomas Knudsen <thokn@sdfe.dk>
2017-11-25 05:46:19 +0100

    Safeguard pj_derivs

--- src/pj_deriv.c
+++ src/pj_deriv.c
@@ -6,6 +6,8 @@ int pj_deriv(LP lp, double h, const PJ *P, struct DERIVS *der) {
     XY t;
     /* get rid of constness until we can do it for real */
     PJ *Q = (PJ *) P;
+    if (0==Q->fwd)
+        return 1;
 
     lp.lam += h;
     lp.phi += h;
@@ -21,6 +23,7 @@ int pj_deriv(LP lp, double h, const PJ *P, struct DERIVS *der) {
     der->y_p = t.y;
     der->x_p = t.x;
     der->y_l = t.y;
+
     lp.phi -= h;
     if (fabs(lp.phi) > M_HALFPI)
         return 1;
@@ -33,6 +36,7 @@ int pj_deriv(LP lp, double h, const PJ *P, struct DERIVS *der) {
     der->y_p -= t.y;
     der->x_p -= t.x;
     der->y_l += t.y;
+
     lp.lam -= h;
     t = (*Q->fwd)(lp, Q);
     if (t.x == HUGE_VAL)
@@ -42,6 +46,7 @@ int pj_deriv(LP lp, double h, const PJ *P, struct DERIVS *der) {
     der->y_p -= t.y;
     der->x_p -= t.x;
     der->y_l -= t.y;
+
     lp.phi += h;
     t = (*Q->fwd)(lp, Q);
     if (t.x == HUGE_VAL)
@@ -51,7 +56,9 @@ int pj_deriv(LP lp, double h, const PJ *P, struct DERIVS *der) {
     der->y_p += t.y;
     der->x_p += t.x;
     der->y_l -= t.y;
-    der->x_l /= (h += h);
+
+    h += h;
+    der->x_l /= h;
     der->y_p /= h;
     der->x_p /= h;
     der->y_l /= h;
