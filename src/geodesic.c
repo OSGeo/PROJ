d83971aa739e421c8d73e08189f274dc0eb29f3d
Charles Karney <charles@karney.com>
2018-02-23 11:46:47 -0500

    Add tests to improve the code coverage.
    Now all of geodesic.c is covered except for 3 lines (and 2 of these are
    intentionally dead code).
        
    This corresponds to tag v1.49.2-c in the GeographicLib code base.
    
    While testing polygons which encircle the globe multiple times, I
    uncovered a problem where the range of the area was not reduced to the
    allowed range (either [0, area0) or (-area0/2, area0/2]) correctly.
    Since the documentation explicity restricted the calculation of polygon
    areas to simple polygons, we'll defer fixing this for now.  (However the
    intention was always to handle the area "algebraically" so that, for
    example, a "bowtie" has zero area.  So I will plan on fixing this for
    1.50.)
    
    Update copyright dates + NEWS.

--- src/geodesic.c
+++ src/geodesic.c
@@ -18,7 +18,7 @@
  *
  * See the comments in geodesic.h for documentation.
  *
- * Copyright (c) Charles Karney (2012-2017) <charles@karney.com> and licensed
+ * Copyright (c) Charles Karney (2012-2018) <charles@karney.com> and licensed
  * under the MIT/X11 License.  For more information, see
  * https://geographiclib.sourceforge.io/
  */
--- src/geodesic.h
+++ src/geodesic.h
@@ -107,7 +107,7 @@
  * twice about restructuring the internals of the C code since this may make
  * porting fixes from the C++ code more difficult.
  *
- * Copyright (c) Charles Karney (2012-2017) <charles@karney.com> and licensed
+ * Copyright (c) Charles Karney (2012-2018) <charles@karney.com> and licensed
  * under the MIT/X11 License.  For more information, see
  * https://geographiclib.sourceforge.io/
  *
