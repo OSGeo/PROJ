3cc7b4c62382650f9bf00365b25f8fa7f6eed603
Even Rouault <even.rouault@spatialys.com>
2018-03-12 23:44:04 +0100

    Add a Travis CLang Static Analyzer target

---
+++ travis/csa/before_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+./travis/before_install.sh
+
+wget http://releases.llvm.org/6.0.0/clang+llvm-6.0.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+tar xJf clang+llvm-6.0.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
