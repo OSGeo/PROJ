7203150301f0a42d27ca1d0181eaa43da0847f55
Kristian Evers <kristianevers@gmail.com>
2018-03-20 16:27:37 +0100

    Change update_man script so that files aren't accidentally committted

--- scripts/update_man.sh
+++ scripts/update_man.sh
@@ -3,5 +3,9 @@ cd docs
 make man
 cd ..
 cp -r docs/build/man/*.1 man/man1
-git add man/man1/*.1
+git add man/man1/proj.1
+git add man/man1/cs2cs.1
+git add man/man1/cct.1
+git add man/man1/geod.1
+git add man/man1/gie.1
 git commit -m "Update man-pages from Sphinx-docs"
