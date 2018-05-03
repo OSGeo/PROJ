41f72905802f30ada06f0cb6b84aba7fbdcff016
Kurt Schwehr <schwehr@google.com>
2018-05-03 11:48:48 -0700

    Improved cleanup of pj_phi2.c based on review comments
    - Doc text by @cffk
    - Merged rework by @busstoptaktik
      - Expand eccnth inline
      - Use the for to handle i
      - Return phi from inside the loop
      - Return HUGE_VAL in the error or ? case.
    - Moved constants into pj_phi2 so they are not separated by the comments
      Not sure if it is easier to follow
    
    Still needs an explanation of the HUGE_VAL return.

--- src/pj_phi2.c
+++ src/pj_phi2.c
@@ -1,26 +1,48 @@
-/* determine latitude angle phi-2 */
+/* Determine latitude angle phi-2. */
 
 #include <math.h>
 
 #include "projects.h"
 
-static const double TOL = 1.0e-10;
-static const int N_ITER = 15;
+/*****************************************************************************/
+double pj_phi2(projCtx ctx, double ts, double e) {
+/******************************************************************************
+Determine latitude angle phi-2.
 
-double
-pj_phi2(projCtx ctx, double ts, double e) {
-    const double eccnth = 0.5 * e;
+Inputs:
+  ts = exp(-psi) where psi is the isometric latitude (dimensionless)
+  e = eccentricity of the ellipsoid (dimensionless)
+Output:
+  phi = geographic latitude (radians)
+
+Here isometric latitude is defined by
+
+  psi = log( tan(pi/4 + phi/2) *
+             ( (1 - e*sin(phi)) / (1 + e*sin(phi)) )^(e/2) )
+      = asinh(tan(phi)) - e * atanh(e * sin(phi))
+
+This routine inverts this relation using the iterative scheme given
+by Snyder (1987), Eqs. (7-9) - (7-11)
+*******************************************************************************/
+    const double TOL = 1.0e-10;
+    const int N_ITER = 15;
     double phi = M_HALFPI - 2.0 * atan(ts);
-    int i = N_ITER;
-    for(;;) {
+    int i;
+
+    for (i = N_ITER;  i > 0;  i--) {
         const double con = e * sin(phi);
-        const double dphi = M_HALFPI - 2.0 * atan(ts * pow((1.0 - con) /
-           (1.0 + con), eccnth)) - phi;
+        const double dphi =
+            M_HALFPI -
+            2.0 * atan(ts * pow((1.0 - con) / (1.0 + con), 0.5 * e)) - phi;
+
         phi += dphi;
-        if( !(fabs(dphi) > TOL && --i) )
-            break;
+
+        if (fabs(dphi) < TOL)
+            return phi;
     }
-    if (i <= 0)
+
+    if (i == 0)
         pj_ctx_set_errno(ctx, PJD_ERR_NON_CON_INV_PHI2);
-    return phi;
+
+    return HUGE_VAL;
 }
