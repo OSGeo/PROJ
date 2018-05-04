1760dafbabb12a6486a928c8d5e8a376a2fe5271
Kurt Schwehr <schwehr@google.com>
2018-05-03 22:59:30 -0700

    Minor cleanup of pj_phi2.c
    - tabs -> spaces
    - IWYU
    - #define -> static const typed
    - Combine definition and initialization
    - Define one var per statement
    - Use the defined symbol rather than embedding bare literals
      -18 -> PJD_ERR_NON_CON_INV_PHI2
    
    Subset of fixes discussed in #971

--- src/pj_phi2.c
+++ src/pj_phi2.c
@@ -1,28 +1,30 @@
-/* determine latitude angle phi-2 */
+/* Determine latitude angle phi-2. */
+
+#include <math.h>
+
 #include "projects.h"
 
-#define TOL 1.0e-10
-#define N_ITER 15
+static const double TOL = 1.0e-10;
+static const int N_ITER = 15;
 
-	double
-pj_phi2(projCtx ctx, double ts, double e) {
-	double eccnth, Phi, con;
-	int i;
+double pj_phi2(projCtx ctx, double ts, double e) {
+    double eccnth = .5 * e;
+    double Phi = M_HALFPI - 2. * atan(ts);
+    double con;
+    int i = N_ITER;
 
-	eccnth = .5 * e;
-	Phi = M_HALFPI - 2. * atan (ts);
-	i = N_ITER;
-	for(;;) {
-		double dphi;
-		con = e * sin (Phi);
-		dphi = M_HALFPI - 2. * atan (ts * pow((1. - con) /
-		   (1. + con), eccnth)) - Phi;
-		Phi += dphi;
-		if( fabs(dphi) > TOL && --i )
-			continue;
-                break;
-	}
-	if (i <= 0)
-		pj_ctx_set_errno( ctx, -18 );
-	return Phi;
+    for(;;) {
+        con = e * sin(Phi);
+        double dphi = M_HALFPI - 2. * atan(ts * pow((1. - con) /
+           (1. + con), eccnth)) - Phi;
+
+        Phi += dphi;
+
+        if (fabs(dphi) > TOL && --i)
+            continue;
+        break;
+    }
+    if (i <= 0)
+        pj_ctx_set_errno(ctx, PJD_ERR_NON_CON_INV_PHI2);
+    return Phi;
 }
