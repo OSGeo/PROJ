8e53bdeae08ef8c1a2f0dbf4921b8aa7cc41aa41
Kurt Schwehr <schwehr@google.com>
2018-05-04 22:44:44 -0700

    Add documentation to pj_phi2 by cffk@

--- src/pj_phi2.c
+++ src/pj_phi2.c
@@ -7,7 +7,22 @@
 static const double TOL = 1.0e-10;
 static const int N_ITER = 15;
 
+/*****************************************************************************/
 double pj_phi2(projCtx ctx, double ts, double e) {
+/******************************************************************************
+Determine latitude angle phi-2.
+Inputs:
+  ts = exp(-psi) where psi is the isometric latitude (dimensionless)
+  e = eccentricity of the ellipsoid (dimensionless)
+Output:
+  phi = geographic latitude (radians)
+Here isometric latitude is defined by
+  psi = log( tan(pi/4 + phi/2) *
+             ( (1 - e*sin(phi)) / (1 + e*sin(phi)) )^(e/2) )
+      = asinh(tan(phi)) - e * atanh(e * sin(phi))
+This routine inverts this relation using the iterative scheme given
+by Snyder (1987), Eqs. (7-9) - (7-11)
+*******************************************************************************/
     double eccnth = .5 * e;
     double Phi = M_HALFPI - 2. * atan(ts);
     double con;
