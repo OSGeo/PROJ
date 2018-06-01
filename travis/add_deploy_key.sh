524a335e0b80cf4ba8c68bf72321405a2803fca4
Even Rouault <even.rouault@spatialys.com>
2018-06-01 23:27:17 +0200

    Update encrypted projdocs-private.key.enc

--- travis/add_deploy_key.sh
+++ travis/add_deploy_key.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-openssl aes-256-cbc -K $encrypted_a32de4b30e70_key -iv $encrypted_a32de4b30e70_iv -in travis/projdocs-private.key.enc -out travis/projdocs-private.key -d
+openssl aes-256-cbc -K $encrypted_e3bb6b75f43b_key -iv $encrypted_e3bb6b75f43b_iv -in travis/projdocs-private.key.enc -out travis/projdocs-private.key -d
 cp travis/projdocs-private.key ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 echo -e "Host *\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
