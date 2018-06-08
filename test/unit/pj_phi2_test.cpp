a8e9607fa9ee414bf25e6dabb9a06fab2c9d014c
Kurt Schwehr <schwehr@google.com>
2018-06-08 12:46:16 -0700

    pj_phi2_test: isnan -> std::isnan

--- test/unit/pj_phi2_test.cpp
+++ test/unit/pj_phi2_test.cpp
@@ -52,12 +52,12 @@ TEST(PjPhi2Test, Basic) {
     // https://www.gnu.org/software/libc/manual/html_node/Mathematical-Constants.html
 
     EXPECT_DOUBLE_EQ(-0.95445818456292697, pj_phi2(ctx, M_PI, 0.0));
-    EXPECT_TRUE(isnan(pj_phi2(ctx, 0.0, M_PI)));
+    EXPECT_TRUE(std::isnan(pj_phi2(ctx, 0.0, M_PI)));
     EXPECT_DOUBLE_EQ(4.0960508381527205, pj_phi2(ctx, -M_PI, 0.0));
-    EXPECT_TRUE(isnan(pj_phi2(ctx, 0.0, -M_PI)));
+    EXPECT_TRUE(std::isnan(pj_phi2(ctx, 0.0, -M_PI)));
 
-    EXPECT_TRUE(isnan(pj_phi2(ctx, M_PI, M_PI)));
-    EXPECT_TRUE(isnan(pj_phi2(ctx, -M_PI, -M_PI)));
+    EXPECT_TRUE(std::isnan(pj_phi2(ctx, M_PI, M_PI)));
+    EXPECT_TRUE(std::isnan(pj_phi2(ctx, -M_PI, -M_PI)));
 }
 
 
@@ -65,21 +65,21 @@ TEST(PjPhi2Test, AvoidUndefinedBehavior) {
     auto ctx = pj_get_default_ctx();
 
     constexpr auto nan = std::numeric_limits<double>::quiet_NaN();
-    EXPECT_TRUE(isnan(pj_phi2(ctx, nan, 0.0)));
-    EXPECT_TRUE(isnan(pj_phi2(ctx, 0.0, nan)));
-    EXPECT_TRUE(isnan(pj_phi2(ctx, nan, nan)));
+    EXPECT_TRUE(std::isnan(pj_phi2(ctx, nan, 0.0)));
+    EXPECT_TRUE(std::isnan(pj_phi2(ctx, 0.0, nan)));
+    EXPECT_TRUE(std::isnan(pj_phi2(ctx, nan, nan)));
 
     // We do not really care about the values that follow.
     constexpr auto inf = std::numeric_limits<double>::infinity();
 
     EXPECT_DOUBLE_EQ(-M_PI_2, pj_phi2(ctx, inf, 0.0));
-    EXPECT_TRUE(isnan(pj_phi2(ctx, 0.0, inf)));
+    EXPECT_TRUE(std::isnan(pj_phi2(ctx, 0.0, inf)));
 
     EXPECT_DOUBLE_EQ(4.7123889803846897, pj_phi2(ctx, -inf, 0.0));
-    EXPECT_TRUE(isnan(pj_phi2(ctx, 0.0, -inf)));
+    EXPECT_TRUE(std::isnan(pj_phi2(ctx, 0.0, -inf)));
 
-    EXPECT_TRUE(isnan(pj_phi2(ctx, inf, inf)));
-    EXPECT_TRUE(isnan(pj_phi2(ctx, -inf, -inf)));
+    EXPECT_TRUE(std::isnan(pj_phi2(ctx, inf, inf)));
+    EXPECT_TRUE(std::isnan(pj_phi2(ctx, -inf, -inf)));
 }
 
 }  // namespace
