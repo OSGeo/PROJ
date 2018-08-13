fe13c01125c57f1a2cd9256f78ee6233891fcec0
Howard Butler <howard@hobu.co>
2018-08-13 09:41:19 -0500

    Add Dockerfile for osgeo/proj-docs image

--- travis/docker.sh
+++ travis/docker.sh
@@ -2,7 +2,7 @@
 
 # proj-docs image has all of the Sphinx
 # dependencies need to build PROJ's docs
-# it is build from docker/docbuild/Dockerfile
+# it is build from docs/docbuild/Dockerfile
 # and is manually pushed as needed to DockerHub
 
 docker pull osgeo/proj-docs
