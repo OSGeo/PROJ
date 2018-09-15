46828f19e4dff0af3683a3d1f79db8278d553e4f
Kristian Evers <kristianevers@gmail.com>
2018-09-15 17:03:28 +0200

    Bump version numbers after 5.2.0 release

--- src/pj_release.c
+++ src/pj_release.c
@@ -11,7 +11,7 @@ char const pj_release[] =
     STR(PROJ_VERSION_MAJOR)"."
     STR(PROJ_VERSION_MINOR)"."
     STR(PROJ_VERSION_PATCH)", "
-    "September 15th, 2018";
+    "March 1st, 2019";
 
 const char *pj_get_release() {
     return pj_release;
--- src/proj.h
+++ src/proj.h
@@ -130,8 +130,8 @@ extern "C" {
 #endif
 
 /* The version numbers should be updated with every release! **/
-#define PROJ_VERSION_MAJOR 5
-#define PROJ_VERSION_MINOR 2
+#define PROJ_VERSION_MAJOR 6
+#define PROJ_VERSION_MINOR 0
 #define PROJ_VERSION_PATCH 0
 
 extern char const pj_release[]; /* global release id string */
