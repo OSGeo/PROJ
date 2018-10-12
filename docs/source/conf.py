b31baceeb84ceb429f682a4430eff92d11218694
Kristian Evers <kristianevers@gmail.com>
2018-10-12 10:56:16 +0200

    Exclude option-includes to avoid blocking warnings in doc builds

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -85,7 +85,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['operations/options/*.rst']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
