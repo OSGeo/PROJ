93ad13380119de80882f47c147baaf64f0270631
Even Rouault <even.rouault@spatialys.com>
2018-10-28 12:08:55 +0100

    Make 'make html' in docs/ run scripts/doxygen.sh when needed

--- travis/build_docs.sh
+++ travis/build_docs.sh
@@ -6,8 +6,6 @@ if test "$TRAVIS_BUILD_DIR" = ""; then
     TRAVIS_BUILD_DIR=$PWD
 fi
 
-./scripts/doxygen.sh
-
 cd docs
 echo "building docs for $TRAVIS_BUILD_DIR/docs"
 docker run --user $(id -u):$(id -g) -v $TRAVIS_BUILD_DIR:/data -w /data/docs osgeo/proj-docs make html
