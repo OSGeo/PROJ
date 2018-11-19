229bc49f7ad1bd024fc503f68d6705f3b0adcdf2
Even Rouault <even.rouault@spatialys.com>
2018-11-19 12:38:10 +0100

    Database: add mapping to GEOID12B height models added per https://github.com/OSGeo/proj-datumgrid/pull/39

--- data/sql/grid_alternatives.sql
+++ data/sql/grid_alternatives.sql
@@ -10,26 +10,26 @@
 
 INSERT INTO grid_packages VALUES ('proj-datumgrid',
                                   'Package with grids of general interest',
-                                  'https://download.osgeo.org/proj/proj-datumgrid-1.8.zip',
+                                  'https://download.osgeo.org/proj/proj-datumgrid-latest.zip',
                                   1,
                                   1);
 
 INSERT INTO grid_packages VALUES ('proj-datumgrid-north-america',
                                   'Package with grids of interest for North-America',
-                                  'https://download.osgeo.org/proj/proj-datumgrid-north-america-1.1.zip',
+                                  'https://download.osgeo.org/proj/proj-datumgrid-north-america-latest.zip',
                                   1,
                                   1);
 
 INSERT INTO grid_packages VALUES ('proj-datumgrid-europe',
                                   'Package with grids of interest for Europe',
-                                  'https://download.osgeo.org/proj/proj-datumgrid-europe-1.1.zip',
+                                  'https://download.osgeo.org/proj/proj-datumgrid-europe-latest.zip',
                                   1,
                                   1);
 
  -- not released yet at the time of writing
 INSERT INTO grid_packages VALUES ('proj-datumgrid-world',
                                   'Package with grids of global extent (too large to be included in proj-datumgrid)',
-                                  'https://download.osgeo.org/proj/proj-datumgrid-world-1.0.zip',
+                                  'https://download.osgeo.org/proj/proj-datumgrid-world-latest.zip',
                                   1,
                                   1);
 
@@ -237,6 +237,90 @@ INSERT INTO grid_alternatives(original_grid_name,
                               'vgridshift',
                               0,         -- do not reverse here as grid_transformation_custom does reference from VerticalCRS height to Geographic height
                               'proj-datumgrid-europe',
+                              NULL, NULL, NULL, NULL);
+
+----------------------------
+-- US GEOID12B height models
+----------------------------
+
+-- CONUS
+INSERT INTO grid_alternatives(original_grid_name,
+                              proj_grid_name,
+                              proj_grid_format,
+                              proj_method,
+                              inverse_direction,
+                              package_name,
+                              url, direct_download, open_license, directory)
+                      VALUES ('g2012Bu0.bin',
+                              'g2012bu0.gtx',
+                              'GTX',
+                              'vgridshift',
+                              0,
+                              'proj-datumgrid-north-america',
+                              NULL, NULL, NULL, NULL);
+
+-- Alaska
+INSERT INTO grid_alternatives(original_grid_name,
+                              proj_grid_name,
+                              proj_grid_format,
+                              proj_method,
+                              inverse_direction,
+                              package_name,
+                              url, direct_download, open_license, directory)
+                      VALUES ('g2012Ba0.bin',
+                              'g2012ba0.gtx',
+                              'GTX',
+                              'vgridshift',
+                              0,
+                              'proj-datumgrid-north-america',
+                              NULL, NULL, NULL, NULL);
+
+-- Puerto Rico
+INSERT INTO grid_alternatives(original_grid_name,
+                              proj_grid_name,
+                              proj_grid_format,
+                              proj_method,
+                              inverse_direction,
+                              package_name,
+                              url, direct_download, open_license, directory)
+                      VALUES ('g2012Bp0.bin',
+                              'g2012bp0.gtx',
+                              'GTX',
+                              'vgridshift',
+                              0,
+                              'proj-datumgrid-north-america',
+                              NULL, NULL, NULL, NULL);
+
+-- Guam
+INSERT INTO grid_alternatives(original_grid_name,
+                              proj_grid_name,
+                              proj_grid_format,
+                              proj_method,
+                              inverse_direction,
+                              package_name,
+                              url, direct_download, open_license, directory)
+                      VALUES ('g2012Bg0.bin',
+                              'g2012bg0.gtx',
+                              'GTX',
+                              'vgridshift',
+                              0,
+                              'proj-datumgrid-north-america',
+                              NULL, NULL, NULL, NULL);
+
+-- American Samoa
+INSERT INTO grid_alternatives(original_grid_name,
+                              proj_grid_name,
+                              proj_grid_format,
+                              proj_method,
+                              inverse_direction,
+                              package_name,
+                              url, direct_download, open_license, directory)
+                      VALUES ('g2012Bs0.bin',
+                              'g2012bs0.gtx',
+                              'GTX',
+                              'vgridshift',
+                              0,
+                              'proj-datumgrid-north-america',
                               NULL, NULL, NULL, NULL);
 
 
