2620701a4bb51a20d49e869acddce2167ba791d0
Even Rouault <even.rouault@spatialys.com>
2019-02-20 22:16:13 +0100

    proj.db: add custom entry for 'RH2000 height to SWEREF99'

--- data/sql/grid_transformation_custom.sql
+++ data/sql/grid_transformation_custom.sql
@@ -1,5 +1,7 @@
 -- This file is hand generated.
 
+-- Denmark
+
 INSERT INTO "grid_transformation" VALUES(
     'PROJ','EPSG_5799_TO_EPSG_4937','DVR90 height to ETRS89',
     NULL,NULL,
@@ -20,4 +22,17 @@ INSERT INTO "grid_transformation" VALUES(
     'EPSG','3237', -- area of use: Denmark onshore
     NULL,
     'EPSG','8666','Geoid (height correction) model file','dnn.gtx',
+    NULL,NULL,NULL,NULL,NULL,NULL,0);
+
+-- Sweden
+
+INSERT INTO "grid_transformation" VALUES(
+    'PROJ','EPSG_5613_TO_EPSG_4377','RH2000 height to SWEREF99',
+    NULL,NULL,
+    'PROJ','HEIGHT_TO_GEOGRAPHIC3D','GravityRelatedHeight to Geographic3D',
+    'EPSG','5613', -- source CRS (RH2000 height)
+    'EPSG','4377', -- target CRS (SWEREF99)
+    'EPSG','3313', -- area of use: Sweden onshore
+    NULL,
+    'EPSG','8666','Geoid (height correction) model file','SWEN17_RH2000.gtx',
     NULL,NULL,NULL,NULL,NULL,NULL,0);
