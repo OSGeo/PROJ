c3dc231ceb57026ec1ebbb3310c333d876d97c28
Even Rouault <even.rouault@spatialys.com>
2019-03-21 12:19:06 +0100

    Travis: fix apt-get update issue (can be removed later)

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -2,3 +2,10 @@
 
 # All platform-specific before_install scripts starts by running this
 # "global" before_install script.
+
+set -e
+
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+    # Workaround apt-get update issue (can be removed later)
+    sudo rm -f /etc/apt/sources.list.d/cassandra*
+fi
