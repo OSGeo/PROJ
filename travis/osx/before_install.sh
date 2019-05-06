b4d9dd437163ecf9c7bf7f16aa4db23321882944
Even Rouault <even.rouault@spatialys.com>
2019-05-06 21:25:05 +0200

    data/Makefile.am: add detection of md5sum utility, and install md5sha1sum on MacOSX Travis

--- travis/osx/before_install.sh
+++ travis/osx/before_install.sh
@@ -8,6 +8,7 @@ brew update
 brew install ccache
 brew install sqlite3
 brew install doxygen
+brew install md5sha1sum
 
 export PATH=$HOME/Library/Python/2.7/bin:$PATH
 # breathe=4.12.0 is the last version to work for us with sphinx 1.8.5 / Python 2
