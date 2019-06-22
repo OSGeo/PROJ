11a53b7c0255087c2b324b36147deb837902dfb9
Even Rouault <even.rouault@spatialys.com>
2019-06-22 21:52:28 +0000

    +proj=ob_tran +o_proj=longlat: set right unit to PJ_IO_UNITS_WHATEVER so as to be able to chain with unitconvert (fixes #1525)

--- src/projections/ob_tran.cpp
+++ src/projections/ob_tran.cpp
@@ -242,7 +242,7 @@ PJ *PROJECTION(ob_tran) {
     /* Support some rather speculative test cases, where the rotated projection */
     /* is actually latlong. We do not want scaling in that case... */
     if (Q->link->right==PJ_IO_UNITS_RADIANS)
-        P->right = PJ_IO_UNITS_PROJECTED;
+        P->right = PJ_IO_UNITS_WHATEVER;
 
 
     return P;
