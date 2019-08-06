b285484ea3b6fb62f0ead9876458f894d6ba3194
Even Rouault <even.rouault@spatialys.com>
2019-08-06 19:45:40 +0200

    Disable website deployment for branch 5.2

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -6,15 +6,3 @@ set -e
 coveralls --extension .c --exclude build_autoconf --exclude build_cmake
 echo "$TRAVIS_SECURE_ENV_VARS"
 ./travis/build_docs.sh
-# Only build and publish docs when changes are made on most recent maintenance branch
-if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "5.2"; then
-    echo "publish website";
-    ./travis/add_deploy_key.sh;
-    ./travis/deploy_website.sh $TRAVIS_BUILD_DIR/docs/build /tmp;
-
-    # Disabled: see discussion of https://github.com/OSGeo/proj.4/pull/1035
-    # if test "$BUILD_NAME" = "linux_gcc"; then
-    #     ./scripts/gen_html_coverage.sh
-    #     ./travis/deploy_html_coverage.sh
-    # fi
-fi
