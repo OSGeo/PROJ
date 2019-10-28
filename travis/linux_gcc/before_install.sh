7026fa2241d4e42ac35688487b7c7dd4d7f8bad1
Even Rouault <even.rouault@spatialys.com>
2019-10-28 09:03:59 +0100

    travis/linux_gcc/before_install.sh: use cppcheck from distribution

--- travis/linux_gcc/before_install.sh
+++ travis/linux_gcc/before_install.sh
@@ -8,15 +8,8 @@ set -e
 sudo apt-get install -qq \
         lcov \
         doxygen graphviz \
-        sqlite3 libsqlite3-dev
-
-# Install Cppcheck to maintain version 1.61
-LIBTINYXML=libtinyxml2-0.0.0_0~git20120518.1.a2ae54e-1_amd64.deb
-CPPCHECK=cppcheck_1.61-1_amd64.deb
-wget -q http://security.ubuntu.com/ubuntu/pool/universe/t/tinyxml2/$LIBTINYXML
-wget -q http://security.ubuntu.com/ubuntu/pool/universe/c/cppcheck/$CPPCHECK
-sudo dpkg -i $LIBTINYXML
-sudo dpkg -i $CPPCHECK
+        sqlite3 libsqlite3-dev \
+        cppcheck
 
 scripts/cppcheck.sh
 scripts/doxygen.sh
