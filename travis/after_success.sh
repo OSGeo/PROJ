da953c042e108cfff19874bd1296b746440e6088
Kristian Evers <kristianevers@gmail.com>
2020-01-01 21:47:10 +0100

    disable website builds on commits to 6.2 branch

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -13,9 +13,9 @@ echo "$TRAVIS_SECURE_ENV_VARS"
 ./travis/build_docs.sh
 # Only build and publish docs when changes are made on most recent maintenance branch
 if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "6.2"; then
-    echo "publish website";
-    ./travis/add_deploy_key.sh;
-    ./travis/deploy_website.sh $TRAVIS_BUILD_DIR/docs/build /tmp;
+    #echo "publish website";
+    #./travis/add_deploy_key.sh;
+    #./travis/deploy_website.sh $TRAVIS_BUILD_DIR/docs/build /tmp;
 
     # Disabled: see discussion of https://github.com/OSGeo/proj.4/pull/1035
     # if test "$BUILD_NAME" = "linux_gcc"; then
