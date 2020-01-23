79eed0da28abdc41c996dc55ee231f1fcffe4fd2
Even Rouault <even.rouault@spatialys.com>
2020-01-23 18:31:32 +0100

    Fix wrong use of derivingConversionRef() that caused GDAL bug
    Hopefully final cut at solving the same class of bug that the one
    that affected QGIS in December.
    
    This time, this hit GDAL in the situation of
    https://lists.osgeo.org/pipermail/gdal-dev/2020-January/051500.html
    The bug fix for that particular issue is in
    PROJStringParser::createFromPROJString()
    
    But grepping more in the code base, I could find other potential smelly
    situations (might not be issues, but better be safe than sorry),
    so let's fix them too.
    
    Bottom line is:
    derivingConversionRef() should *only* be used for consultation, and
    never to create a new ProjectedCRS()

--- src/iso19111/factory.cpp
+++ src/iso19111/factory.cpp
@@ -2581,7 +2581,7 @@ AuthorityFactory::createProjectedCRS(const std::string &code) const {
                 pj_add_type_crs_if_needed(text_definition), d->context());
             auto projCRS = dynamic_cast<const crs::ProjectedCRS *>(obj.get());
             if (projCRS) {
-                const auto &conv = projCRS->derivingConversionRef();
+                const auto conv = projCRS->derivingConversion();
                 auto newConv =
                     (conv->nameStr() == "unnamed")
                         ? operation::Conversion::create(
@@ -2602,10 +2602,9 @@ AuthorityFactory::createProjectedCRS(const std::string &code) const {
                     boundCRS->baseCRS().get());
                 if (projCRS) {
                     auto newBoundCRS = crs::BoundCRS::create(
-                        crs::ProjectedCRS::create(
-                            props, projCRS->baseCRS(),
-                            projCRS->derivingConversionRef(),
-                            projCRS->coordinateSystem()),
+                        crs::ProjectedCRS::create(props, projCRS->baseCRS(),
+                                                  projCRS->derivingConversion(),
+                                                  projCRS->coordinateSystem()),
                         boundCRS->hubCRS(), boundCRS->transformation());
                     return NN_NO_CHECK(
                         util::nn_dynamic_pointer_cast<crs::ProjectedCRS>(
