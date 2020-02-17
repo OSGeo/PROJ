2d98931a8875412325680773db85572c383add66
Kristian Evers <kristianevers@gmail.com>
2020-02-17 12:02:35 +0100

    Remove man3 files
    Closes #1258

--- scripts/update_man.sh
+++ scripts/update_man.sh
@@ -9,6 +9,4 @@ git add man/man1/cct.1
 git add man/man1/geod.1
 git add man/man1/gie.1
 git add man/man1/projinfo.1
-git add man/man3/geodesic.3
-git add man/man3/pj_init.3
 git commit -m "Update man-pages from Sphinx-docs"
