73be5b1e68cf467b9d3f5efa0540a457d9d1a0b1
Even Rouault <even.rouault@spatialys.com>
2020-03-01 12:52:24 +0100

    6.3 branch: disable doc publication [ci skip]

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -11,15 +11,3 @@ fi
 
 echo "$TRAVIS_SECURE_ENV_VARS"
 ./travis/build_docs.sh
-# Only build and publish docs when changes are made on most recent maintenance branch
-if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "6.3"; then
-    echo "publish website";
-    ./travis/add_deploy_key.sh;
-    ./travis/deploy_website.sh $TRAVIS_BUILD_DIR/docs/build /tmp;
-
-    # Disabled: see discussion of https://github.com/OSGeo/proj.4/pull/1035
-    # if test "$BUILD_NAME" = "linux_gcc"; then
-    #     ./scripts/gen_html_coverage.sh
-    #     ./travis/deploy_html_coverage.sh
-    # fi
-fi
