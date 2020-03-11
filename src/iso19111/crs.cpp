3a69b7be2fa26c3ffb91eb2a3b86deb199da20c3
Even Rouault <even.rouault@spatialys.com>
2020-03-11 13:19:15 +0100

    ProjectedCRS::identify(): tune it to better work with ESRI WKT representation of EPSG:2193
    Adresses https://github.com/OSGeo/gdal/issues/2303 by raising the
    identification confidence from 25% to 90% (90% means equivalent for
    all purposes, but name not strictly the EPSG official one)

--- src/iso19111/crs.cpp
+++ src/iso19111/crs.cpp
@@ -3743,8 +3743,7 @@ ProjectedCRS::identify(const io::AuthorityFactoryPtr &authorityFactory) const {
                             return res;
                         }
                         res.emplace_back(crsNN, eqName ? 90 : 70);
-                    } else if (crs->nameStr() == thisName &&
-                               CRS::getPrivate()->implicitCS_ &&
+                    } else if (eqName && CRS::getPrivate()->implicitCS_ &&
                                coordinateSystem()
                                    ->axisList()[0]
                                    ->unit()
@@ -3765,7 +3764,8 @@ ProjectedCRS::identify(const io::AuthorityFactoryPtr &authorityFactory) const {
                                    dbContext) &&
                                objects.size() == 1) {
                         res.clear();
-                        res.emplace_back(crsNN, 100);
+                        res.emplace_back(crsNN,
+                                         crs->nameStr() == thisName ? 100 : 90);
                         return res;
                     } else {
                         res.emplace_back(crsNN, 25);
