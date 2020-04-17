b7f5e7ea751588914eec2b64fd2ee39ac2eefc54
Even Rouault <even.rouault@spatialys.com>
2020-04-17 20:37:05 +0200

    ellps_shape: catch corner case where es would be set to NaN. Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=21761

--- src/ell_set.cpp
+++ src/ell_set.cpp
@@ -318,7 +318,8 @@ static int ellps_shape (PJ *P) {
 
     }
 
-    if (P->es < 0)
+    // Written that way to catch NaN
+    if (!(P->es >= 0))
         return proj_errno_set (P, PJD_ERR_ES_LESS_THAN_ZERO);
     return 0;
 }
