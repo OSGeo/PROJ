104011fe6a6e5ab6d97733f293aa5892c0646435
Even Rouault <even.rouault@spatialys.com>
2020-04-25 15:29:47 +0200

    postinstall checks: check that we include installed C++ headers (refs #2182)

---
+++ test/postinstall/testcpp/testcpp.cpp
@@ -0,0 +1,10 @@
+#include <proj/io.hpp>
+#include <proj/crs.hpp>
+#include <proj/coordinateoperation.hpp>
+#include <proj/coordinatesystem.hpp>
+#include <proj/datum.hpp>
+
+int main(int /*argc */, char** /* argv */)
+{
+    return 0;
+}
