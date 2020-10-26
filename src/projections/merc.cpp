75a8436b2491dbc1eebb196ea42b961fd011a52e
Charles Karney <charles.karney@sri.com>
2020-10-26 18:29:19 -0400

    Use sincos optimization in merc_e_forward.  Revised timing data...
    Times per call in ns = nanoseconds.
    
                    Fedora 31    Ubuntu 18
                    g++-9.3.1    g++-7.5.0
                    fwd   inv    fwd   inv
        old merc    207   461    217   522
        new merc    159   457    137   410
          etmerc    212   196    174   147
    
    The new forward method is now 25% faster (resp 35% faster) on Fedora
    31 (resp Ubuntu 18).  The new inverse method is the same speed (resp 20%
    faster) on Fedora 31 (resp Ubuntu 18).
    
    The accuracy is hardly affected: rms error increases from 0.30 nm to
    0.33 nm, max error increases from 1.83 nm to 1.84 nm (a barely
    noticeable degradation).

--- src/projections/merc.cpp
+++ src/projections/merc.cpp
@@ -13,7 +13,8 @@ PROJ_HEAD(webmerc, "Web Mercator / Pseudo Mercator") "\n\tCyl, Ell\n\t";
 static PJ_XY merc_e_forward (PJ_LP lp, PJ *P) {          /* Ellipsoidal, forward */
     PJ_XY xy = {0.0,0.0};
     xy.x = P->k0 * lp.lam;
-    xy.y = P->k0 * (asinh(tan(lp.phi)) - P->e * atanh(P->e * sin(lp.phi)));
+    double sphi = sin(lp.phi), cphi = cos(lp.phi);
+    xy.y = P->k0 * (asinh(sphi/cphi) - P->e * atanh(P->e * sphi));
     return xy;
 }
 
