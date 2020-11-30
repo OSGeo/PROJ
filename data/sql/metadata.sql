eef73e3eca2959768563d2dbb95a591c68b1d807
Even Rouault <even.rouault@spatialys.com>
2020-11-30 14:00:46 +0100

    Database: add metadata with the version number of the database layout and check it in the code
    This is aimed at detecting running a PROJ version against a proj.db that is not
    meant to work with it. This happens sometimes in complex setups mixing PROJ
    versions. Hopefully this will help spotting the issue earlier.

--- data/sql/metadata.sql
+++ data/sql/metadata.sql
@@ -1,2 +1,13 @@
+-- Version of the database structure.
+-- The major number indicates an incompatible change (e.g. table or column
+-- removed or renamed).
+-- The minor number is incremented if a backward compatible change done, that
+-- is the new database can still work with an older PROJ version.
+-- When updating those numbers, the DATABASE_LAYOUT_VERSION_MAJOR and
+-- DATABASE_LAYOUT_VERSION_MINOR constants in src/iso19111/factory.cpp must be
+-- updated as well.
+INSERT INTO "metadata" VALUES('DATABASE.LAYOUT.VERSION.MAJOR', 1);
+INSERT INTO "metadata" VALUES('DATABASE.LAYOUT.VERSION.MINOR', 0);
+
 INSERT INTO "metadata" VALUES('EPSG.VERSION', 'v10.007');
 INSERT INTO "metadata" VALUES('EPSG.DATE', '2020-11-18');
