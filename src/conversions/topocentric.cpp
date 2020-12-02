ebe00831b7d6de3aae01ddd5f3074e53ac6d981e
Even Rouault <even.rouault@spatialys.com>
2020-12-02 15:22:48 +0100

    fix build

--- src/conversions/topocentric.cpp
+++ src/conversions/topocentric.cpp
@@ -76,7 +76,7 @@ static PJ_COORD topocentric_inv(PJ_COORD in, PJ * P)
 /*********************************************************************/
 PJ *CONVERSION(topocentric,1) {
 /*********************************************************************/
-    struct pj_opaque *Q = static_cast<struct pj_opaque*>(pj_calloc (1, sizeof (struct pj_opaque)));
+    struct pj_opaque *Q = static_cast<struct pj_opaque*>(calloc (1, sizeof (struct pj_opaque)));
     if (nullptr==Q)
         return pj_default_destructor (P, ENOMEM);
     P->opaque = static_cast<void *>(Q);
