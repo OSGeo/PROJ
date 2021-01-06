f909fc05e1820ba13de2bfd7056872343c13d59a
Kristian Evers <kristianevers@gmail.com>
2021-01-06 10:45:46 +0100

    Update version numbers for website

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -69,8 +69,8 @@ copyright = u'1983-{0}'.format(now.year)
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
-version = '7.2.0'
-data_version = '1.3'
+version = '7.2.1'
+data_version = '1.4'
 
 # use same |release| as |version|
 release = version
