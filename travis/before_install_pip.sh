5a88099cba4ac875a9a644eac5302ff6f73ac350
Mike Taves <mwtoews@gmail.com>
2021-02-07 21:59:27 +1300

    CI: specify pip < 21.0; avoid using pip3 script

--- travis/before_install_pip.sh
+++ travis/before_install_pip.sh
@@ -4,7 +4,7 @@
 # "global" before_install script.
 
 # Configure Python pip
-pip3 install --user --upgrade pip
-echo `pip3 --version`
-pip3 config --user set global.progress_bar off
-pip3 install --user jsonschema
+python3 -m pip install --user --upgrade "pip < 21.0"
+echo `python3 -m pip --version`
+python3 -m pip config --user set global.progress_bar off
+python3 -m pip install --user jsonschema
