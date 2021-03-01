43407f7ba46fbefe71d84c23911f16982c14154a
Kristian Evers <kristianevers@gmail.com>
2021-03-01 13:04:22 +0100

    Dont' build website from 7.2 branch any more

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -14,8 +14,8 @@ echo "$TRAVIS_SECURE_ENV_VARS"
 # Only build and publish docs when changes are made on most recent maintenance branch
 if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "7.2"; then
     echo "publish website";
-    ./travis/add_deploy_key.sh;
-    ./travis/deploy_website.sh $TRAVIS_BUILD_DIR/docs/build /tmp;
+    #./travis/add_deploy_key.sh;
+    #./travis/deploy_website.sh $TRAVIS_BUILD_DIR/docs/build /tmp;
 
     # Disabled: see discussion of https://github.com/OSGeo/proj.4/pull/1035
     # if test "$BUILD_NAME" = "linux_gcc"; then
