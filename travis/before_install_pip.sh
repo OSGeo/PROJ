cc2d1758acedd2886241967be355b88a7b9d09fe
Mike Taves <mwtoews@gmail.com>
2021-03-11 23:57:58 +1300

    CI: specify pip < 21.0

--- travis/before_install_pip.sh
+++ travis/before_install_pip.sh
@@ -4,7 +4,7 @@
 # "global" before_install script.
 
 # Configure Python pip
-pip3 install --user --upgrade pip
+pip3 install --user --upgrade "pip < 21.0"
 echo `pip3 --version`
 pip3 config --user set global.progress_bar off
 pip3 install --user jsonschema
