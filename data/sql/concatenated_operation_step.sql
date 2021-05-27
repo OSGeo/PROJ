548a6ab0509ea03bf4fffc377c17fcd0ba977c7d
Even Rouault <even.rouault@spatialys.com>
2021-05-27 11:49:06 +0200

    Database: update to EPSG v10.024

--- data/sql/concatenated_operation_step.sql
+++ data/sql/concatenated_operation_step.sql
@@ -439,3 +439,5 @@ INSERT INTO "concatenated_operation_step" VALUES('EPSG','9687',1,'EPSG','8447');
 INSERT INTO "concatenated_operation_step" VALUES('EPSG','9687',2,'EPSG','8448');
 INSERT INTO "concatenated_operation_step" VALUES('EPSG','9731',1,'EPSG','9729');
 INSERT INTO "concatenated_operation_step" VALUES('EPSG','9731',2,'EPSG','9726');
+INSERT INTO "concatenated_operation_step" VALUES('EPSG','9750',1,'EPSG','9727');
+INSERT INTO "concatenated_operation_step" VALUES('EPSG','9750',2,'EPSG','9726');
