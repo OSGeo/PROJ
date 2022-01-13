a3a147b4996c2dc47d2e1c4d7260fbc493783e21
Even Rouault <even.rouault@spatialys.com>
2022-01-13 12:09:53 +0100

    travis/install.sh: adapt for removal of autoconf

--- travis/install.sh
+++ travis/install.sh
@@ -16,86 +16,78 @@ fi
 echo "NPROC=${NPROC}"
 export MAKEFLAGS="-j ${NPROC}"
 
-# prepare build files
-./autogen.sh
+# For some odd reason the tar xzvf $TAR_FILENAME doesn't work on Travis-CI ...
+if test "$TRAVIS" = ""; then
+    # Run dist target
+    mkdir build_cmake
+    cd build_cmake
+    cmake -DBUILD_TESTING=OFF ..
+    make dist
+    TAR_FILENAME=`ls *.tar.gz`
+    echo "TAR_FILENAME=$TAR_FILENAME"
+    TAR_DIRECTORY=`basename $TAR_FILENAME .tar.gz`
+    echo "TAR_DIRECTORY=$TAR_DIRECTORY"
+    tar xzvf $TAR_FILENAME
 
-# autoconf build
-mkdir build_autoconf
-cd build_autoconf
-../configure
-make dist-all >/dev/null
-# Check consistency of generated tarball
-TAR_FILENAME=`ls *.tar.gz`
-TAR_DIRECTORY=`basename $TAR_FILENAME .tar.gz`
-tar xvzf $TAR_FILENAME
-cd ..
-
-# compare with CMake's dist
-mkdir build_cmake
-cd build_cmake
-cmake -DBUILD_TESTING=OFF ..
-make dist
-tar xzf $TAR_FILENAME
-cd ..
-diff -qr build_autoconf/$TAR_DIRECTORY build_cmake/$TAR_DIRECTORY || true
-
-# continue build from autoconf
-cd build_autoconf/$TAR_DIRECTORY
+    # continue build from dist tarball
+    cd $TAR_DIRECTORY
+fi
 
 # There's a nasty #define CS in a Solaris system header. Avoid being caught about that again
 CXXFLAGS="-DCS=do_not_use_CS_for_solaris_compat $CXXFLAGS"
 
-# autoconf build from generated tarball
-mkdir build_autoconf
-cd build_autoconf
-../configure --prefix=/tmp/proj_autoconf_install_from_dist_all
+# build from generated tarball
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=/tmp/proj_install_from_dist_all
 
 make >/dev/null
 
-if [ "$(uname)" == "Linux" -a -f src/.libs/libproj.so ]; then
-if objdump -TC "src/.libs/libproj.so" | grep "elf64-x86-64">/dev/null; then
+if [ "$(uname)" == "Linux" -a -f bin/libproj.so ]; then
+if objdump -TC "bin/libproj.so" | grep "elf64-x86-64">/dev/null; then
     echo "Checking exported symbols..."
     cat $TRAVIS_BUILD_DIR/scripts/reference_exported_symbols.txt | sort > /tmp/reference_exported_symbols.txt
-    $TRAVIS_BUILD_DIR/scripts/dump_exported_symbols.sh src/.libs/libproj.so | sort > /tmp/got_symbols.txt
-    diff -u /tmp/reference_exported_symbols.txt /tmp/got_symbols.txt || (echo "Difference(s) found in exported symbols. If intended, refresh scripts/reference_exported_symbols.txt with 'scripts/dump_exported_symbols.sh src/.libs/libproj.so > scripts/reference_exported_symbols.txt'"; exit 1)
+    $TRAVIS_BUILD_DIR/scripts/dump_exported_symbols.sh bin/libproj.so | sort > /tmp/got_symbols.txt
+    diff -u /tmp/reference_exported_symbols.txt /tmp/got_symbols.txt || (echo "Difference(s) found in exported symbols. If intended, refresh scripts/reference_exported_symbols.txt with 'scripts/dump_exported_symbols.sh bin/libproj.so > scripts/reference_exported_symbols.txt'"; exit 1)
 fi
 fi
 
-make check
+ctest
 make install
-find /tmp/proj_autoconf_install_from_dist_all
+find /tmp/proj_install_from_dist_all
 if [ $BUILD_NAME = "linux_gcc" ]; then
-    $TRAVIS_BUILD_DIR/test/postinstall/test_autotools.sh /tmp/proj_autoconf_install_from_dist_all shared
-    $TRAVIS_BUILD_DIR/test/postinstall/test_autotools.sh /tmp/proj_autoconf_install_from_dist_all static
+    $TRAVIS_BUILD_DIR/test/postinstall/test_autotools.sh /tmp/proj_install_from_dist_all shared
+    # We should do a static build to make below work:
+    # $TRAVIS_BUILD_DIR/test/postinstall/test_autotools.sh /tmp/proj_install_from_dist_all static
 elif [ $BUILD_NAME = "osx" ]; then
     # skip static builds, as macOS shows: ld: unknown option: -Bstatic
-    $TRAVIS_BUILD_DIR/test/postinstall/test_autotools.sh /tmp/proj_autoconf_install_from_dist_all shared
+    $TRAVIS_BUILD_DIR/test/postinstall/test_autotools.sh /tmp/proj_install_from_dist_all shared
 else
     echo "Skipping test_autotools.sh test for $BUILD_NAME"
 fi
 
-/tmp/proj_autoconf_install_from_dist_all/bin/projinfo EPSG:32631 -o PROJJSON -q > out.json
+/tmp/proj_install_from_dist_all/bin/projinfo EPSG:32631 -o PROJJSON -q > out.json
 cat out.json
 echo "Validating JSON"
-jsonschema -i out.json /tmp/proj_autoconf_install_from_dist_all/share/proj/projjson.schema.json && echo "Valid !"
+jsonschema -i out.json /tmp/proj_install_from_dist_all/share/proj/projjson.schema.json && echo "Valid !"
 
-/tmp/proj_autoconf_install_from_dist_all/bin/projinfo EPSG:4326+3855 -o PROJJSON -q > out.json
+/tmp/proj_install_from_dist_all/bin/projinfo EPSG:4326+3855 -o PROJJSON -q > out.json
 cat out.json
 echo "Validating JSON"
-jsonschema -i out.json /tmp/proj_autoconf_install_from_dist_all/share/proj/projjson.schema.json && echo "Valid !"
+jsonschema -i out.json /tmp/proj_install_from_dist_all/share/proj/projjson.schema.json && echo "Valid !"
 
-/tmp/proj_autoconf_install_from_dist_all/bin/projinfo "+proj=longlat +ellps=GRS80 +nadgrids=@foo +type=crs" -o PROJJSON -q > out.json
+/tmp/proj_install_from_dist_all/bin/projinfo "+proj=longlat +ellps=GRS80 +nadgrids=@foo +type=crs" -o PROJJSON -q > out.json
 cat out.json
 echo "Validating JSON"
-jsonschema -i out.json /tmp/proj_autoconf_install_from_dist_all/share/proj/projjson.schema.json && echo "Valid !"
-/tmp/proj_autoconf_install_from_dist_all/bin/projinfo @out.json -o PROJJSON -q > out2.json
+jsonschema -i out.json /tmp/proj_install_from_dist_all/share/proj/projjson.schema.json && echo "Valid !"
+/tmp/proj_install_from_dist_all/bin/projinfo @out.json -o PROJJSON -q > out2.json
 diff -u out.json out2.json
 
-/tmp/proj_autoconf_install_from_dist_all/bin/projinfo -s EPSG:3111 -t GDA2020 -o PROJJSON -o PROJJSON -q > out.json
+/tmp/proj_install_from_dist_all/bin/projinfo -s EPSG:3111 -t GDA2020 -o PROJJSON -o PROJJSON -q > out.json
 cat out.json
 echo "Validating JSON"
-jsonschema -i out.json /tmp/proj_autoconf_install_from_dist_all/share/proj/projjson.schema.json && echo "Valid !"
-/tmp/proj_autoconf_install_from_dist_all/bin/projinfo @out.json -o PROJJSON -q > out2.json
+jsonschema -i out.json /tmp/proj_install_from_dist_all/share/proj/projjson.schema.json && echo "Valid !"
+/tmp/proj_install_from_dist_all/bin/projinfo @out.json -o PROJJSON -q > out2.json
 diff -u out.json out2.json
 
 # Test make clean target
@@ -105,14 +97,15 @@ cd ..
 
 if [ $TRAVIS_OS_NAME != "osx" ]; then
     # Check that we can retrieve the resource directory in a relative way after renaming the installation prefix
-    mkdir /tmp/proj_autoconf_install_from_dist_all_renamed
-    mv /tmp/proj_autoconf_install_from_dist_all /tmp/proj_autoconf_install_from_dist_all_renamed/subdir
-    LD_LIBRARY_PATH=/tmp/proj_autoconf_install_from_dist_all_renamed/subdir/lib /tmp/proj_autoconf_install_from_dist_all_renamed/subdir/bin/projsync --source-id ? --dry-run --system-directory || /bin/true
-    LD_LIBRARY_PATH=/tmp/proj_autoconf_install_from_dist_all_renamed/subdir/lib /tmp/proj_autoconf_install_from_dist_all_renamed/subdir/bin/projsync --source-id ? --dry-run --system-directory 2>/dev/null | grep "Downloading from https://cdn.proj.org into /tmp/proj_autoconf_install_from_dist_all_renamed/subdir/share/proj"
-    sed -i '1cprefix=/tmp/proj_autoconf_install_from_dist_all_renamed/subdir' /tmp/proj_autoconf_install_from_dist_all_renamed/subdir/lib/pkgconfig/proj.pc
+    mkdir /tmp/proj_install_from_dist_all_renamed
+    mv /tmp/proj_install_from_dist_all /tmp/proj_install_from_dist_all_renamed/subdir
+    LD_LIBRARY_PATH=/tmp/proj_install_from_dist_all_renamed/subdir/lib /tmp/proj_install_from_dist_all_renamed/subdir/bin/projsync --source-id ? --dry-run --system-directory || /bin/true
+    LD_LIBRARY_PATH=/tmp/proj_install_from_dist_all_renamed/subdir/lib /tmp/proj_install_from_dist_all_renamed/subdir/bin/projsync --source-id ? --dry-run --system-directory 2>/dev/null | grep "Downloading from https://cdn.proj.org into /tmp/proj_install_from_dist_all_renamed/subdir/share/proj"
+    sed -i '1cprefix=/tmp/proj_install_from_dist_all_renamed/subdir' /tmp/proj_install_from_dist_all_renamed/subdir/lib/pkgconfig/proj.pc
     if [ $BUILD_NAME = "linux_gcc" ]; then
-        $TRAVIS_BUILD_DIR/test/postinstall/test_autotools.sh /tmp/proj_autoconf_install_from_dist_all_renamed/subdir shared
-        PROJ_LIB=/tmp/proj_autoconf_install_from_dist_all_renamed/subdir/share/proj $TRAVIS_BUILD_DIR/test/postinstall/test_autotools.sh /tmp/proj_autoconf_install_from_dist_all_renamed/subdir static
+        $TRAVIS_BUILD_DIR/test/postinstall/test_autotools.sh /tmp/proj_install_from_dist_all_renamed/subdir shared
+        # We should do a static build to make below work:
+        # PROJ_LIB=/tmp/proj_install_from_dist_all_renamed/subdir/share/proj $TRAVIS_BUILD_DIR/test/postinstall/test_autotools.sh /tmp/proj_install_from_dist_all_renamed/subdir static
     else
         echo "Skipping test_autotools.sh test for $BUILD_NAME"
     fi
@@ -181,20 +174,22 @@ if [ "$BUILD_NAME" != "linux_gcc8" -a "$BUILD_NAME" != "linux_gcc_32bit" ]; then
     /tmp/proj_cmake_install_renamed/subdir/bin/projsync --source-id ? --dry-run --system-directory 2>/dev/null | grep "Downloading from https://cdn.proj.org into /tmp/proj_cmake_install_renamed/subdir/share/proj"
     set -e
 
-    # return to root
-    cd ../..
+    if test "$TRAVIS" = ""; then
+        # return to root
+        cd ../..
+    fi
 
     # There's an issue with the clang on Travis + coverage + cpp code
     if [ "$BUILD_NAME" != "linux_clang" ]; then
         # autoconf build with grids and coverage
         if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-            CFLAGS="--coverage" CXXFLAGS="--coverage" ./configure;
+            CFLAGS="--coverage" CXXFLAGS="--coverage" cmake . -DCMAKE_BUILD_TYPE=Debug
         else
-            CFLAGS="$CFLAGS --coverage" CXXFLAGS="$CXXFLAGS --coverage" LDFLAGS="$LDFLAGS -lgcov" ./configure;
+            CFLAGS="$CFLAGS --coverage" CXXFLAGS="$CXXFLAGS --coverage" LDFLAGS="$LDFLAGS -lgcov" cmake . -DCMAKE_BUILD_TYPE=Debug
         fi
     else
-        ./configure
+        cmake . -DCMAKE_BUILD_TYPE=Debug
     fi
     make >/dev/null
-    make check
+    ctest
 fi
