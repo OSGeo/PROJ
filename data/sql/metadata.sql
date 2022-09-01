ae64b7f5f2cb08a931e036f9330251b94594bd30
Even Rouault <even.rouault@spatialys.com>
2022-09-01 13:32:24 +0200

    Merge pull request #3307 from rouault/epsg_10_076
    Database: update to EPSG v10.076

--- data/sql/metadata.sql
+++ data/sql/metadata.sql
@@ -9,8 +9,8 @@
 INSERT INTO "metadata" VALUES('DATABASE.LAYOUT.VERSION.MAJOR', 1);
 INSERT INTO "metadata" VALUES('DATABASE.LAYOUT.VERSION.MINOR', 2);
 
-INSERT INTO "metadata" VALUES('EPSG.VERSION', 'v10.074');
-INSERT INTO "metadata" VALUES('EPSG.DATE', '2022-08-01');
+INSERT INTO "metadata" VALUES('EPSG.VERSION', 'v10.076');
+INSERT INTO "metadata" VALUES('EPSG.DATE', '2022-08-31');
 
 -- The value of ${PROJ_VERSION} is substituted at build time by the actual
 -- value.
