OSGeo/PROJ

commit 4abde1b9f8fdc5fce1d7a3179d5bcae6520ae396

cs2cs: when source (or target) CRS is specified by name, promote it to 3D if there's a known 3D CRS of same name and that the other CRS is 3D

src/apps/cs2cs.cpp
@@ -332,6 +332,19 @@ static std::string get_geog_crs_proj_string_from_proj_crs(PJ *src,
     return ret;
 }
 
+// ---------------------------------------------------------------------------
+
+static bool is3DCRS(const PJ* crs) {
+    auto type = proj_get_type(crs);
+    if( type == PJ_TYPE_COMPOUND_CRS )
+        return true;
+    if( type == PJ_TYPE_GEOGRAPHIC_3D_CRS )
+        return true;
+    if( type == PJ_TYPE_GEODETIC_CRS || type == PJ_TYPE_PROJECTED_CRS )
+        return proj_cs_get_axis_count(nullptr, crs) == 3;
+    return false;
+}
+
 /************************************************************************/
 /*                                main()                                */
 /************************************************************************/
@@ -807,6 +820,39 @@ int main(int argc, char **argv) {
         if( dst3D ) {
             proj_destroy(dst);
             dst = dst3D;
+        }
+    } else {
+        // Auto-promote source/target CRS if it is specified by its name,
+        // if it has a known 3D version of it and that the other CRS is 3D.
+        // e.g cs2cs "WGS 84 + EGM96 height" "WGS 84"
+        if (is3DCRS(dst) && !is3DCRS(src) &&
+            proj_get_id_code(src, 0) != nullptr &&
+            Identifier::isEquivalentName(fromStr.c_str(),
+                                         proj_get_name(src))) {
+            auto promoted = proj_crs_promote_to_3D(nullptr, nullptr, src);
+            if (promoted)
+            {
+                if (proj_get_id_code(promoted, 0) != nullptr) {
+                    proj_destroy(src);
+                    src = promoted;
+                } else {
+                    proj_destroy(promoted);
+                }
+            }
+        } else if (is3DCRS(src) && !is3DCRS(dst) &&
+                   proj_get_id_code(dst, 0) != nullptr &&
+                   Identifier::isEquivalentName(toStr.c_str(),
+                                                proj_get_name(dst))) {
+            auto promoted = proj_crs_promote_to_3D(nullptr, nullptr, dst);
+            if (promoted)
+            {
+                if (proj_get_id_code(promoted, 0) != nullptr) {
+                    proj_destroy(dst);
+                    dst = promoted;
+                } else {
+                    proj_destroy(promoted);
+                }
+            }
         }
     }
 

test/cli/testvarious
@@ -1033,6 +1033,16 @@ $EXE -E EPSG:4326 "NAD83(HARN)" >> ${OUT} <<EOF
 34 -120
 EOF
 
+echo  "##############################################################" >> ${OUT}
+echo  "Check that we promote CRS specified by name to 3D when the other one is 3D" >> ${OUT}
+$EXE -d 3 -E "WGS 84" "WGS 84 + EGM96 height" >> ${OUT} <<EOF
+49 2 50
+EOF
+
+$EXE -d 3 -E "WGS 84 + EGM96 height" "WGS 84" >> ${OUT} <<EOF
+49 2 0
+EOF
+
 
 # Done!
 # do 'diff' with distribution results

test/cli/tv_out.dist
@@ -500,3 +500,7 @@ Check that we can use a transformation spanning the antimeridian (should use Pul
 ##############################################################
 Check that we select the operation that has the smallest area of use, when 2 have the same accuracy
 34 -120	33d59'59.983"N	119d59'59.955"W 0.000
+##############################################################
+Check that we promote CRS specified by name to 3D when the other one is 3D
+49 2 50	49.000	2.000 4.936
+49 2 0	49.000	2.000 45.064

commit 47f13f34c6aabdb2ff2a3bb75f62229b727c92ee

cs2cs: fix incorrect use of proj_cs_get_axis_count() introduced in 4abde1b9f8fdc5fce1d7a3179d5bcae6520ae396 (not in a release)

src/apps/cs2cs.cpp
@@ -341,7 +341,13 @@ static bool is3DCRS(const PJ* crs) {
     if( type == PJ_TYPE_GEOGRAPHIC_3D_CRS )
         return true;
     if( type == PJ_TYPE_GEODETIC_CRS || type == PJ_TYPE_PROJECTED_CRS )
-        return proj_cs_get_axis_count(nullptr, crs) == 3;
+    {
+        auto cs = proj_crs_get_coordinate_system(nullptr, crs);
+        assert(cs);
+        const bool ret = proj_cs_get_axis_count(nullptr, cs) == 3;
+        proj_destroy(cs);
+        return ret;
+    }
     return false;
 }
 