1ea45fdb547febe77dfa0615761ef90d154a8019
Jonathan Metzman <metzman@chromium.org>
2022-10-13 16:28:00 -0400

    test

--- .github/workflows/mac/before_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-mamba install --yes --quiet compilers
-mamba install --yes --quiet python=3.8 autoconf automake libtool ccache jsonschema
-mamba install --yes --quiet --only-deps proj=7.1.1=h45baca5_3
