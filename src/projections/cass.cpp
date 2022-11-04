9284d36fed0e1252095990ac2dce38259390fa15
Even Rouault <even.rouault@spatialys.com>
2022-11-04 22:59:05 +0100

    cass: use iterative generic inversion to make GIGS 5108 roundtripping tests succeed
    I've modified the inverse method to use the generic
    inversion method to improve its accuracy, which make it possible to
    enable the 5108.gie tests that were previously failing on the 1000
    iterations roundtripping.

--- src/projections/cass.cpp
+++ src/projections/cass.cpp
@@ -82,13 +82,13 @@ static PJ_LP cass_e_inverse (PJ_XY xy, PJ *P) {          /* Ellipsoidal, inverse
     lp.lam = D * (1. + T1 * D2 *
         (-C4 + (1. + 3. * T1) * D2 * C5)) / cos (phi1);
 
-    if( Q->hyperbolic )
-    {
-        // EPSG guidance note 7-2 suggests a custom approximation for the
-        // 'Vanua Levu 1915 / Vanua Levu Grid' case, but better use the
-        // generic inversion method
-        lp = pj_generic_inverse_2d(xy, P, lp);
-    }
+    // EPSG guidance note 7-2 suggests a custom approximation for the
+    // 'Vanua Levu 1915 / Vanua Levu Grid' case, but better use the
+    // generic inversion method
+    // Actually use it in the non-hyperbolic case. It enables to make the
+    // 5108.gie roundtripping tests to success, with at most 2 iterations.
+    constexpr double deltaXYTolerance = 1e-12;
+    lp = pj_generic_inverse_2d(xy, P, lp, deltaXYTolerance);
 
     return lp;
 }
