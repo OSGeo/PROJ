ce95d2eea474b002f794ec39a4fec34c23155969
Even Rouault <even.rouault@spatialys.com>
2022-12-14 14:47:12 +0100

    Database: add concatenated operations for NADCON5 transformations

--- data/sql_filelist.cmake
+++ data/sql_filelist.cmake
@@ -37,6 +37,7 @@ set(SQL_FILES
   "${SQL_DIR}/iau.sql"
   "${SQL_DIR}/grid_alternatives.sql"
   "${SQL_DIR}/grid_alternatives_generated_noaa.sql"
+  "${SQL_DIR}/nadcon5_concatenated_operations.sql"
   "${SQL_DIR}/customizations.sql"
   "${SQL_DIR}/nkg_post_customizations.sql"
   "${SQL_DIR}/commit.sql"
