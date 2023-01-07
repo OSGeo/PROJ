e82223d13121d3f13fb7afd20613fd4424ebcfa3
Howard Butler <hobu.inc@gmail.com>
2023-01-07 15:00:02 -0600

    update dirs

--- docs/rtd/post_build.sh
+++ docs/rtd/post_build.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 #
 
-BUILDDIR="/home/docs/checkouts/readthedocs.org/user_builds/$READTHEDOCS_PROJECT/checkouts/$READTHEDOCS_VERSION/docs"
+BUILDDIR="/home/docs/checkouts/readthedocs.org/user_builds/$READTHEDOCS_PROJECT/checkouts/$READTHEDOCS_VERSION"
 
 pwd
+#/home/docs/checkouts/readthedocs.org/user_builds/osgeo-proj/checkouts/3538
 #/home/docs/checkouts/readthedocs.org/user_builds/osgeo-proj/checkouts/3538
 
 echo "updating CPP files and copying schemas for BUILDDIR: $BUILDDIR"
 
 ls $BUILDDIR
 
-BUILDDIR="/home/docs/checkouts/readthedocs.org/user_builds/$READTHEDOCS_PROJECT/$READTHEDOCS_VERSION/docs/build"
+BUILDDIR="/home/docs/checkouts/readthedocs.org/user_builds/$READTHEDOCS_PROJECT/$READTHEDOCS_VERSION/build"
 
 sed "s/io::Convention_/io::Convention/g" < ${BUILDDIR}/html/development/reference/cpp/io.html | sed "s/>Convention_/>Convention/g" | sed "s/_WKT2/WKT2/g" | sed "s/_WKT1/WKT1/g" > ${BUILDDIR}/html/development/reference/cpp/io.html.tmp
 mv ${BUILDDIR}/html/development/reference/cpp/io.html.tmp ${BUILDDIR}/html/development/reference/cpp/io.html
