192972ca9ea186240156f5ed01df99669fb46d97
Howard Butler <hobu.inc@gmail.com>
2023-01-07 17:28:37 -0600

    lint and doc cancel/skip

--- docs/rtd/post_build.sh
+++ docs/rtd/post_build.sh
@@ -9,20 +9,30 @@ pwd
 
 echo "listing BUILDDIR: $BUILDDIR"
 
+
 ls $BUILDDIR
+
+find .
 
 echo "listing BUILDDIR: $BUILDDIR/docs"
 
 ls $BUILDDIR/docs
 
-echo "listing BUILDDIR: $BUILDDIR/docs/_build"
+echo "listing BUILDDIR: $BUILDDIR/docs/build"
 
-ls $BUILDDIR/docs/_build
+ls $BUILDDIR/docs/build
 
-BUILDDIR="/home/docs/checkouts/readthedocs.org/user_builds/$READTHEDOCS_PROJECT/$READTHEDOCS_VERSION/docs/_build"
-echo "list BUILDDIR: $BUILDDIR"
+echo "listing BUILDDIR: $BUILDDIR/docs/build"
+
+ls $BUILDDIR/
+
+BUILDDIR="/home/docs/checkouts/readthedocs.org/user_builds/$READTHEDOCS_PROJECT/$READTHEDOCS_VERSION/docs/build"
+
 ls $BUILDDIR
 
+echo "listing BUILDDIR: $BUILDDIR/docs/build"
+
+ls $BUILDDIR/docs/build
 sed "s/io::Convention_/io::Convention/g" < ${BUILDDIR}/html/development/reference/cpp/io.html | sed "s/>Convention_/>Convention/g" | sed "s/_WKT2/WKT2/g" | sed "s/_WKT1/WKT1/g" > ${BUILDDIR}/html/development/reference/cpp/io.html.tmp
 mv ${BUILDDIR}/html/development/reference/cpp/io.html.tmp ${BUILDDIR}/html/development/reference/cpp/io.html
 cp -r ../schemas ${BUILDDIR}/html/schemas
