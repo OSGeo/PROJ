6adcf9e7b9977fa4aff8af50cebcc1c2ff99d355
Howard Butler <hobu.inc@gmail.com>
2023-01-07 15:23:19 -0600

    are we here now?

--- docs/rtd/post_build.sh
+++ docs/rtd/post_build.sh
@@ -11,7 +11,8 @@ echo "updating CPP files and copying schemas for BUILDDIR: $BUILDDIR"
 
 ls $BUILDDIR
 
-BUILDDIR="/home/docs/checkouts/readthedocs.org/user_builds/$READTHEDOCS_PROJECT/$READTHEDOCS_VERSION/build"
+BUILDDIR="/home/docs/checkouts/readthedocs.org/user_builds/$READTHEDOCS_PROJECT/$READTHEDOCS_VERSION/docs/build"
+ls $BUILDDIR
 
 sed "s/io::Convention_/io::Convention/g" < ${BUILDDIR}/html/development/reference/cpp/io.html | sed "s/>Convention_/>Convention/g" | sed "s/_WKT2/WKT2/g" | sed "s/_WKT1/WKT1/g" > ${BUILDDIR}/html/development/reference/cpp/io.html.tmp
 mv ${BUILDDIR}/html/development/reference/cpp/io.html.tmp ${BUILDDIR}/html/development/reference/cpp/io.html
