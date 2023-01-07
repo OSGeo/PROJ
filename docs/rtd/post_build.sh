e084d3c573c66af861f5c46dc5b958a356ba7018
Howard Butler <hobu.inc@gmail.com>
2023-01-07 15:46:06 -0600

    way more lint

--- docs/rtd/post_build.sh
+++ docs/rtd/post_build.sh
@@ -7,11 +7,20 @@ pwd
 #/home/docs/checkouts/readthedocs.org/user_builds/osgeo-proj/checkouts/3538
 #/home/docs/checkouts/readthedocs.org/user_builds/osgeo-proj/checkouts/3538
 
-echo "updating CPP files and copying schemas for BUILDDIR: $BUILDDIR"
+echo "listing BUILDDIR: $BUILDDIR"
 
 ls $BUILDDIR
 
-BUILDDIR="/home/docs/checkouts/readthedocs.org/user_builds/$READTHEDOCS_PROJECT/$READTHEDOCS_VERSION/docs/build"
+echo "listing BUILDDIR: $BUILDDIR/docs"
+
+ls $BUILDDIR/docs
+
+echo "listing BUILDDIR: $BUILDDIR/docs/_build"
+
+ls $BUILDDIR/docs/_build
+
+BUILDDIR="/home/docs/checkouts/readthedocs.org/user_builds/$READTHEDOCS_PROJECT/$READTHEDOCS_VERSION/docs/_build"
+echo "list BUILDDIR: $BUILDDIR"
 ls $BUILDDIR
 
 sed "s/io::Convention_/io::Convention/g" < ${BUILDDIR}/html/development/reference/cpp/io.html | sed "s/>Convention_/>Convention/g" | sed "s/_WKT2/WKT2/g" | sed "s/_WKT1/WKT1/g" > ${BUILDDIR}/html/development/reference/cpp/io.html.tmp
