eddbdaf6517a1294be98e68c22ff44ae9e6ebfce
Mike Taves <mwtoews@gmail.com>
2023-01-25 11:34:35 +1300

    remove debugging for post_build.sh

--- docs/rtd/post_build.sh
+++ docs/rtd/post_build.sh
@@ -1,24 +1,16 @@
 #!/bin/sh
-set -ex
+set -e
 
-pwd
-tree -d
-# content location from https://github.com/readthedocs/readthedocs.org/issues/9172
-CONTENTLOCATION="../../artifacts/${READTHEDOCS_VERSION}/sphinx"
-# and later adjusted to https://github.com/readthedocs/readthedocs.org/pull/9888
-echo "READTHEDOCS_OUTPUT=${READTHEDOCS_OUTPUT}"
+# https://github.com/readthedocs/readthedocs.org/pull/9888
 READTHEDOCS_OUTPUT="_readthedocs"
-tree -d ${READTHEDOCS_OUTPUT}
-find ${READTHEDOCS_OUTPUT}
+find ${READTHEDOCS_OUTPUT} -type d
 READTHEDOCS_OUTPUT_HTML="${READTHEDOCS_OUTPUT}/html"
-tree ${READTHEDOCS_OUTPUT_HTML}
-find ${READTHEDOCS_OUTPUT_HTML}
 
 # Undoes the hack done in scripts/doxygen.sh
 sed "s/io::Convention_/io::Convention/g" < ${READTHEDOCS_OUTPUT_HTML}/development/reference/cpp/io.html | sed "s/>Convention_/>Convention/g" | sed "s/_WKT2/WKT2/g" | sed "s/_WKT1/WKT1/g" > ${READTHEDOCS_OUTPUT_HTML}/development/reference/cpp/io.html.tmp
 mv ${READTHEDOCS_OUTPUT_HTML}/development/reference/cpp/io.html.tmp ${READTHEDOCS_OUTPUT_HTML}/development/reference/cpp/io.html
 
-# Copy JSON schemata for public consumption
+# Copy JSON schemas for public consumption
 READTHEDOCS_OUTPUT_SCHEMAS="${READTHEDOCS_OUTPUT_HTML}/schemas"
 cp -r schemas ${READTHEDOCS_OUTPUT_SCHEMAS}
 cp ./data/triangulation.schema.json ${READTHEDOCS_OUTPUT_SCHEMAS}
