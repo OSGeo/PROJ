e18784c1a4fd4383a2a324691e0347eacb57953a
Mike Taves <mwtoews@gmail.com>
2023-01-25 20:28:17 +1300

    ReadTheDocs: update output directory for artefacts, other updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -194,7 +194,6 @@ html_context = {
     'theme_vcs_pageview_mode': 'edit',
     'github_user': 'OSGeo',
     'github_repo': 'PROJ',
-    # TODO: edit when switching active branch
     'github_version': '/7.0/docs/source/',
 }
 
