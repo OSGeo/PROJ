aa1afbdeb7e4da83cff16aef4cc620b813f561d9
Mike Taves <mwtoews@gmail.com>
2023-01-25 15:36:13 +1300

    ReadTheDocs: update output directory for artefacts, other updates

--- docs/rtd/post_build.sh
+++ docs/rtd/post_build.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 set -e
 
-# content location from https://github.com/readthedocs/readthedocs.org/issues/9172
-CONTENTLOCATION="../../artifacts/${READTHEDOCS_VERSION}/sphinx"
-
-find ${CONTENTLOCATION}
+# https://github.com/readthedocs/readthedocs.org/pull/9888
+READTHEDOCS_OUTPUT="_readthedocs"
+find ${READTHEDOCS_OUTPUT} -type d
+READTHEDOCS_OUTPUT_HTML="${READTHEDOCS_OUTPUT}/html"
 
 # Undoes the hack done in scripts/doxygen.sh
-sed "s/io::Convention_/io::Convention/g" < ${CONTENTLOCATION}/development/reference/cpp/io.html | sed "s/>Convention_/>Convention/g" | sed "s/_WKT2/WKT2/g" | sed "s/_WKT1/WKT1/g" > ${CONTENTLOCATION}/development/reference/cpp/io.html.tmp
-mv ${CONTENTLOCATION}/development/reference/cpp/io.html.tmp ${CONTENTLOCATION}/development/reference/cpp/io.html
+sed "s/io::Convention_/io::Convention/g" < ${READTHEDOCS_OUTPUT_HTML}/development/reference/cpp/io.html | sed "s/>Convention_/>Convention/g" | sed "s/_WKT2/WKT2/g" | sed "s/_WKT1/WKT1/g" > ${READTHEDOCS_OUTPUT_HTML}/development/reference/cpp/io.html.tmp
+mv ${READTHEDOCS_OUTPUT_HTML}/development/reference/cpp/io.html.tmp ${READTHEDOCS_OUTPUT_HTML}/development/reference/cpp/io.html
 
-# Copy JSON schemata for public consumption
-cp -r schemas ${CONTENTLOCATION}/schemas
-cp ./data/triangulation.schema.json ${CONTENTLOCATION}/schemas
+# Copy JSON schemas for public consumption
+READTHEDOCS_OUTPUT_SCHEMAS="${READTHEDOCS_OUTPUT_HTML}/schemas"
+cp -r schemas ${READTHEDOCS_OUTPUT_SCHEMAS}
+cp ./data/triangulation.schema.json ${READTHEDOCS_OUTPUT_SCHEMAS}
