2c502c43b9bc31720a12033e2b6b03a6bcd79312
Mike Taves <mwtoews@gmail.com>
2023-01-25 22:29:50 +1300

    ReadTheDocs: use ninja, fix branch ref

--- docs/rtd/pre_build.sh
+++ docs/rtd/pre_build.sh
@@ -4,6 +4,6 @@ set -e
 mkdir build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS=-Wno-error -DCMAKE_INSTALL_PREFIX=$HOME/.local ..
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS=-Wno-error -DCMAKE_INSTALL_PREFIX=$HOME/.local ..
 
-make -j install
+ninja install
