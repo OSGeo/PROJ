e70d845c2ad1e4ad243921685517ee3200b205f5
David Hoese <david.hoese@ssec.wisc.edu>
2023-02-03 12:11:38 -0600

    Remove NAN include in src/apps/proj_strtod.cpp
    Co-authored-by: Even Rouault <even.rouault@spatialys.com>

--- src/apps/proj_strtod.cpp
+++ src/apps/proj_strtod.cpp
@@ -90,7 +90,7 @@ Thomas Knudsen, thokn@sdfe.dk, 2017-01-17/2017-09-18
 #include <ctype.h>
 #include <errno.h>
 #include <float.h>  /* for HUGE_VAL */
-#include <math.h>   /* for pow(), NAN */
+#include <math.h>   /* for pow() */
 #include <stdlib.h> /* for abs */
 #include <string.h> /* for strchr, strncmp */
 
