80a7250eb8a9a436906b0fb079f70286def00fbc
David Hoese <david.hoese@ssec.wisc.edu>
2023-02-05 13:24:29 -0600

    Export ci_starts_with and use for NaN checks

--- src/apps/proj_strtod.cpp
+++ src/apps/proj_strtod.cpp
@@ -85,6 +85,8 @@ Thomas Knudsen, thokn@sdfe.dk, 2017-01-17/2017-09-18
 
 ***********************************************************************/
 
+#define FROM_PROJ_CPP
+
 #include "proj_strtod.h"
 
 #include <ctype.h>
@@ -95,6 +97,10 @@ Thomas Knudsen, thokn@sdfe.dk, 2017-01-17/2017-09-18
 #include <string.h> /* for strchr, strncmp */
 
 #include <limits>
+
+#include "proj/internal/internal.hpp"
+
+using namespace NS_PROJ::internal;
 
 double proj_strtod(const char *str, char **endptr) {
     double number = 0, integral_part = 0;
@@ -126,7 +132,7 @@ double proj_strtod(const char *str, char **endptr) {
     }
 
     /* NaN */
-    if (strncmp(p, "NaN", 3) == 0) {
+    if (ci_starts_with(p, "NaN")) {
         if (endptr)
             *endptr = const_cast<char *>(p + 3);
         return std::numeric_limits<double>::quiet_NaN();
