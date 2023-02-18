22b84838ec498aa1a4d2b7ceb95d72fc7167e869
Even Rouault <even.rouault@spatialys.com>
2023-02-18 19:52:50 +0100

    .github/workflows/mac/before_install.sh: add pkg-config

--- .github/workflows/mac/before_install.sh
+++ .github/workflows/mac/before_install.sh
@@ -3,5 +3,5 @@
 set -e
 
 mamba install --yes --quiet compilers
-mamba install --yes --quiet python=3.8 autoconf automake libtool ccache jsonschema
+mamba install --yes --quiet python=3.8 autoconf automake libtool pkg-config ccache jsonschema
 mamba install --yes --quiet --only-deps proj=7.1.1=h45baca5_3
