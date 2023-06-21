63777bed2a373d9366818142a6c359e9d6d44394
Mike Taves <mwtoews@gmail.com>
2023-06-21 20:52:59 +1200

    Docs: refactor Python code for sphinx configuration and extensions

--- docs/source/_extensions/redirects.py
+++ docs/source/_extensions/redirects.py
@@ -1,39 +1,32 @@
-import os
+"""Sphinx doc extension for HTML redirects.
 
-# https://tech.signavio.com/2017/managing-sphinx-redirects
+Within conf.py, add a 'redirect_files' dict with alias/target pairs.
 
+Originally from https://tech.signavio.com/2017/managing-sphinx-redirects (dead)
+"""
+from pathlib import Path
 
-template="""<html>
+template = """\
+<html>
   <head>
-    <meta http-equiv="refresh" content="1; url=%s" />
+    <meta http-equiv="refresh" content="1; url={target}" />
     <script>
-      window.location.href = "%s"
+      window.location.href = "{target}"
     </script>
   </head>
-</html>"""
+</html>
+"""
 
 
-def gather_redirects():
-    output = {}
-
-    output.update({ 'projjson.html' : 'usage/projjson.html' })
-    return output
-
-from shutil import copyfile
-# copy legacy redirects
-def copy_legacy_redirects(app, docname): # Sphinx expects two arguments
-    if app.builder.name == 'html':
-        for key in app.config.redirect_files:
-            src = key
-            tgt = app.config.redirect_files[key]
-            html = template % (tgt, tgt)
-            with open(os.path.join(app.outdir, src), 'wb') as f:
-                f.write(html.encode('utf-8'))
-                f.close()
-
+def copy_legacy_redirects(app, docname):  # Sphinx expects two arguments
+    """Copy legacy redirects."""
+    if app.builder.name == "html":
+        for alias, target in app.config.redirect_files.items():
+            pth = Path(app.outdir) / alias
+            pth.write_text(template.format(target=target))
 
 
 def setup(app):
-    app.add_config_value('redirect_files', {}, 'html')
-    app.connect('build-finished', copy_legacy_redirects)
-    return { 'parallel_read_safe': False, 'parallel_write_safe': True }
+    app.add_config_value("redirect_files", {}, "html")
+    app.connect("build-finished", copy_legacy_redirects)
+    return {"parallel_read_safe": False, "parallel_write_safe": True}
