d1f955f622b050c0aa9bf6b962f7e582653943c0
Jean-Christophe Malapert <jcmalapert@googlemail.com>
2023-06-26 14:35:30 +0200

    Update build_db_from_iau.py
    Update IAU script according to https://github.com/opengeospatial/Planetary-DWG/blob/main/draft_crs_definitions/proposal001.adoc from issue opengeospatial/Planetary-DWG#5

--- scripts/build_db_from_iau.py
+++ scripts/build_db_from_iau.py
@@ -86,7 +86,7 @@ projection_data = [
     [75, "Lambert Conic Conformal", "Lambert Conic Conformal (2SP)", "Latitude of false origin", 40, "Longitude of false origin", 0, "Latitude of 1st standard parallel", 20, "Latitude of 2nd standard parallel", 60, "Easting at false origin", 0, "Northing at false origin", 0],
     [80, "Lambert Azimuthal Equal Area", "Lambert Azimuthal Equal Area", "Latitude of natural origin", 40, "Longitude of natural origin", 0, "False easting", 0, "False northing", 0,  None, None, None, None],
     [85, "Albers Equal Area", "Albers Equal Area", "Latitude of false origin", 40,"Longitude of false origin", 0,  "Latitude of 1st standard parallel", 20, "Latitude of 2nd standard parallel", 60, "Easting at false origin", 0, "Northing at false origin", 0],
-    [90, "Popular Visualisation Pseudo-Mercator", "Popular Visualisation Pseudo Mercator", "Latitude of natural origin", 0, "Longitude of natural origin", 0, "False easting", 0, "False northing", 0, None, None, None, None],
+    [90, "Mercator", "Mercator (Spherical)", "Latitude of natural origin", 0, "Longitude of natural origin", 0, "False easting", 0, "False northing", 0, None, None, None, None],
 ]
 
 method_and_param_mapping = {
@@ -113,7 +113,7 @@ method_and_param_mapping = {
     "Lambert Azimuthal Equal Area": ["EPSG", 9820],
     "Albers Equal Area" : ["EPSG", 9822],
     "Orthographic" : ["EPSG", 9840],
-    "Popular Visualisation Pseudo Mercator": ["EPSG", 1024]
+    "Mercator (Spherical)": ["EPSG", 1026]
 }
 
 def append_proj_paramater(name, value):
