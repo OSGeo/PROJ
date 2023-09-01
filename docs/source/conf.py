5cecc4855616d21b023c363dedf57598ed1d5adf
Kristian Evers <kreve@sdfi.dk>
2023-09-01 14:22:11 +0200

    docs: update git branch and date

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,8 +33,8 @@ release = "9.3.0"
 # PROJ-data version
 data_version = "1.15"
 
-today_date = date.today()
-# today_date = date(Y, M, D)  # or use a specific date
+#today_date = date.today()
+today_date = date(2023, 9, 1)  # or use a specific date
 today_fmt = "%d %b %Y"
 
 copyright = "1983-{}, PROJ contributors".format(today_date.year)
@@ -140,7 +140,7 @@ html_context = {
     "theme_vcs_pageview_mode": "edit",
     "github_user": "OSGeo",
     "github_repo": "PROJ",
-    "github_version": "master",
+    "github_version": "9.3",
     "conf_py_path": "/docs/source/",
 }
 
