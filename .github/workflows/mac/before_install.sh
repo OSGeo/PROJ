81b136898195aad8f0ac6735c034751b9a6c0d81
Even Rouault <even.rouault@spatialys.com>
2024-01-30 18:55:32 +0100

    CI: add a M1 ARM64 MacOS runner
    Co-authored-by: Howard Butler <hobu.inc@gmail.com>

--- .github/workflows/mac/before_install.sh
+++ .github/workflows/mac/before_install.sh
@@ -2,6 +2,4 @@
 
 set -e
 
-mamba install --yes --quiet compilers
-mamba install --yes --quiet python=3.8 autoconf automake libtool pkg-config ccache jsonschema
-mamba install --yes --quiet --only-deps proj=7.1.1=h45baca5_3
+mamba install --yes --quiet --only-deps proj
