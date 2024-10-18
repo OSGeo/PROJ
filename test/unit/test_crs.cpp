a6f5fa77036a578ece69117bcb7a6aa8341de1d7
Even Rouault <even.rouault@spatialys.com>
2024-10-18 19:09:58 +0200

    Database: ingest deprecated ESRI names to be able to better import old WKT ESRI
    Fixes #4281

--- test/unit/test_crs.cpp
+++ test/unit/test_crs.cpp
@@ -2523,6 +2523,37 @@ TEST(crs, projectedCRS_from_EPSG_as_WKT1_ESRI_s_jtsk03_krovak_east_north) {
 
 // ---------------------------------------------------------------------------
 
+TEST(crs, projectedCRS_from_EPSG_with_deprecated_ESRI_name_as_WKT1_ESRI) {
+    auto dbContext = DatabaseContext::create();
+    auto crs =
+        AuthorityFactory::create(dbContext, "EPSG")->createProjectedCRS("5186");
+
+    // Check we use the non-deprecated ESRI names, so:
+    // "KGD2002_Central_Belt_2010" and not "Korea_2000_Korea_Central_Belt_2010"
+    // "GCS_KGD2002" and not "GCS_Korea_2000"
+    // "D_Korea_Geodetic_Datum_2002" and not "D_Korea_2000"
+    auto esri_wkt =
+        "PROJCS[\"KGD2002_Central_Belt_2010\",GEOGCS[\"GCS_KGD2002\","
+        "DATUM[\"D_Korea_Geodetic_Datum_2002\","
+        "SPHEROID[\"GRS_1980\",6378137.0,298.257222101]],"
+        "PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]],"
+        "PROJECTION[\"Transverse_Mercator\"],"
+        "PARAMETER[\"False_Easting\",200000.0],"
+        "PARAMETER[\"False_Northing\",600000.0],"
+        "PARAMETER[\"Central_Meridian\",127.0],"
+        "PARAMETER[\"Scale_Factor\",1.0],"
+        "PARAMETER[\"Latitude_Of_Origin\",38.0],"
+        "UNIT[\"Meter\",1.0]]";
+
+    EXPECT_EQ(
+        crs->exportToWKT(
+            WKTFormatter::create(WKTFormatter::Convention::WKT1_ESRI, dbContext)
+                .get()),
+        esri_wkt);
+}
+
+// ---------------------------------------------------------------------------
+
 TEST(crs, projectedCRS_as_PROJ_string) {
     auto crs = createProjected();
 
@@ -3466,6 +3497,30 @@ TEST(crs, projectedCRS_identify_db) {
         EXPECT_EQ(res.front().first->identifiers()[0]->code(), "19912");
         EXPECT_EQ(*(res.front().first->identifiers()[0]->codeSpace()),
                   "IAU_2015");
+        EXPECT_EQ(res.front().second, 90);
+    }
+    {
+        // Identify a WKT ESRI using deprecated ESRI names
+        // Cf https://github.com/OSGeo/PROJ/issues/4281
+        auto obj = WKTParser().attachDatabaseContext(dbContext).createFromWKT(
+            "PROJCS[\"Korea_2000_Korea_Central_Belt_2010\","
+            "GEOGCS[\"GCS_Korea_2000\",DATUM[\"D_Korea_2000\","
+            "SPHEROID[\"GRS_1980\",6378137.0,298.257222101]],"
+            "PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]],"
+            "PROJECTION[\"Transverse_Mercator\"],"
+            "PARAMETER[\"False_Easting\",200000.0],"
+            "PARAMETER[\"False_Northing\",600000.0],"
+            "PARAMETER[\"Central_Meridian\",127.0],"
+            "PARAMETER[\"Scale_Factor\",1.0],"
+            "PARAMETER[\"Latitude_Of_Origin\",38.0],"
+            "UNIT[\"Meter\",1.0]]");
+        auto crs = nn_dynamic_pointer_cast<ProjectedCRS>(obj);
+        ASSERT_TRUE(crs != nullptr);
+        auto allFactory = AuthorityFactory::create(dbContext, std::string());
+        auto res = crs->identify(allFactory);
+        ASSERT_GE(res.size(), 1U);
+        EXPECT_EQ(res.front().first->identifiers()[0]->code(), "102082");
+        EXPECT_EQ(*(res.front().first->identifiers()[0]->codeSpace()), "ESRI");
         EXPECT_EQ(res.front().second, 90);
     }
 }
