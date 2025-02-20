9a6641be8b15283daac8a642d335705b90159602
Even Rouault <even.rouault@spatialys.com>
2025-02-20 18:35:32 +0100

    Insert a 'wisdom of the day' in our version string

--- src/release.cpp
+++ src/release.cpp
@@ -6,8 +6,10 @@
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
 
-char const pj_release[] = "Rel. " STR(PROJ_VERSION_MAJOR) "." STR(
-    PROJ_VERSION_MINOR) "." STR(PROJ_VERSION_PATCH) ", "
-                                                    "March 1st, 2025";
+char const pj_release[] =
+    "Rel. " STR(PROJ_VERSION_MAJOR) "." STR(PROJ_VERSION_MINOR) "." STR(
+        PROJ_VERSION_PATCH) ", "
+                            "March 1st, 2025. Our mission: \"Diversity, Equity "
+                            "and Inclusion (DEI) for all CRS of the world.\" ";
 
 const char *pj_get_release() { return pj_release; }
