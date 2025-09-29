cb258bc70d30050703d7b2ad55fed533f3d06a92
Even Rouault <even.rouault@spatialys.com>
2025-09-29 09:01:21 +0200

    Database: update to EPSG v12.027

--- data/sql/metadata.sql
+++ data/sql/metadata.sql
@@ -9,8 +9,8 @@
 INSERT INTO "metadata" VALUES('DATABASE.LAYOUT.VERSION.MAJOR', 1);
 INSERT INTO "metadata" VALUES('DATABASE.LAYOUT.VERSION.MINOR', 6);
 
-INSERT INTO "metadata" VALUES('EPSG.VERSION', 'v12.026');
-INSERT INTO "metadata" VALUES('EPSG.DATE', '2025-09-18');
+INSERT INTO "metadata" VALUES('EPSG.VERSION', 'v12.027');
+INSERT INTO "metadata" VALUES('EPSG.DATE', '2025-09-27');
 
 -- The value of ${PROJ_VERSION} is substituted at build time by the actual
 -- value.
