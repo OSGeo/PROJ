6c120531aebad69ee6cfd9e68999fa9b54d619f8
Even Rouault <even.rouault@spatialys.com>
2025-12-01 01:15:54 +0100

    Database: create ESRI aliases for geodetic_datum and geodetic_crs even if they are the same as EPSG ones
    Otherwise, on ESRI WKT export, we'd add D_ and GCS_ prefixes if there is
    no ESRI alias defined

--- data/sql/show_useless_aliases.sql
+++ data/sql/show_useless_aliases.sql
@@ -3,7 +3,7 @@ SELECT 'Useless alias_name for geodetic_datum...';
 SELECT a.table_name, a.auth_name, a.code, a.alt_name, a.source
         FROM geodetic_datum d JOIN alias_name a WHERE
             a.table_name = 'geodetic_datum' AND a.auth_name = d.auth_name AND
-            a.code = d.code AND d.name = a.alt_name AND a.source != 'EPSG_OLD';
+            a.code = d.code AND d.name = a.alt_name AND a.source NOT IN ('ESRI', 'EPSG_OLD');
 
 SELECT 'Useless alias_name for vertical_datum...';
 
@@ -17,7 +17,7 @@ SELECT 'Useless alias_name for geodetic_crs...';
 SELECT a.table_name, a.auth_name, a.code, a.alt_name, a.source
         FROM geodetic_crs c JOIN alias_name a WHERE
             a.table_name = 'geodetic_crs' AND a.auth_name = c.auth_name AND
-            a.code = c.code AND c.name = a.alt_name AND a.source != 'EPSG_OLD';
+            a.code = c.code AND c.name = a.alt_name AND a.source NOT IN ('ESRI', 'EPSG_OLD');
 
 SELECT 'Useless alias_name for vertical_crs...';
 
