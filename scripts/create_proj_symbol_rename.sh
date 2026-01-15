6555161a24d268e24065a5411d8446d14260a638
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2026-01-15 18:09:06 +0100

    proj_symbol_rename: use pre-built libproj.so
    Use the libproj.so built by the build system
    to extract the list of externally visible
    functions. This ensures the library is built
    in the same way as the release.

--- scripts/create_proj_symbol_rename.sh
+++ scripts/create_proj_symbol_rename.sh
@@ -19,50 +19,47 @@
 # for now, and we would just set the NS_PROJ existing macro to change the C++ base
 # namespace which defaults to osgeo::proj currently.
 
-# To be run from a 'build' subdir after running configure
+# Run as "create_proj_symbol_rename.sh lib/libproj.so".
 
-PROJ_ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/..; pwd)
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+PROJ_ROOT=$(dirname $(readlink -f "${SCRIPT_DIR}"))
 
-rm -rf tmp_alias
-mkdir tmp_alias
-for i in ${PROJ_ROOT}/src/*.c; do
-    echo $i
-    (cd tmp_alias; gcc -fvisibility=hidden ${i} -I../src -I${PROJ_ROOT}/src -I${PROJ_ROOT}/include -fPIC -c)
-done
-for i in ${PROJ_ROOT}/src/*.cpp ${PROJ_ROOT}/src/iso19111/*.cpp ${PROJ_ROOT}/src/iso19111/operation/*.cpp; do
-    echo $i
-    (cd tmp_alias; g++ -fvisibility=hidden -std=c++11 ${i} -I../src -I${PROJ_ROOT}/src -I${PROJ_ROOT}/include -fPIC -c)
-done
+error() {
+    echo "Error: $*"
+    exit 1
+}
 
-(cd tmp_alias; g++ -fvisibility=hidden -std=c++11 *.o -shared -o libproj.so)
+LIBPROJ="$1"
+
+[ -f "$LIBPROJ" ] || error "\"$LIBPROJ\" does not look like path to libproj.so"
 
 OUT_FILE=proj_symbol_rename.h
 
-rm $OUT_FILE 2>/dev/null
+rm -f $OUT_FILE 2>/dev/null
 
 echo "/* This is a generated file by create_proj_symbol_rename.sh. *DO NOT EDIT MANUALLY !* */" >> $OUT_FILE
 echo "#ifndef PROJ_SYMBOL_RENAME_H" >> $OUT_FILE
 echo "#define PROJ_SYMBOL_RENAME_H" >> $OUT_FILE
 
-symbol_list=$(objdump -t tmp_alias/libproj.so   | grep " g     " | grep .text | awk '{print $6}' | grep -v -e _Z | sort)
+symbol_list=$(objdump -t "$LIBPROJ" | grep " g     " | grep .text | awk '{print $6}' | grep -v -e _Z | sort)
 for symbol in $symbol_list
 do
     echo "#define $symbol internal_$symbol" >> $OUT_FILE
 done
 
-rodata_symbol_list=$(objdump -t tmp_alias/libproj.so | grep " g     O \\.rodata" |  awk '{print $6}')
+rodata_symbol_list=$(objdump -t "$LIBPROJ" | grep " g     O \\.rodata" |  awk '{print $6}')
 for symbol in $rodata_symbol_list
 do
     echo "#define $symbol internal_$symbol" >> $OUT_FILE
 done
 
-#data_symbol_list=$(objdump -t tmp_alias/libproj.so | grep "\\.data"  | grep -v -e __dso_handle -e __TMC_END__ | awk '{print $6}' | grep -v "\\."| grep -v _Z)
+#data_symbol_list=$(objdump -t "$LIBPROJ" | grep "\\.data"  | grep -v -e __dso_handle -e __TMC_END__ | awk '{print $6}' | grep -v "\\."| grep -v _Z)
 #for symbol in $data_symbol_list
 #do
 #    echo "#define $symbol internal_$symbol" >> $OUT_FILE
 #done
 
-bss_symbol_list=$(objdump -t tmp_alias/libproj.so  | grep "g     O \\.bss" | awk '{print $6}' | grep -v "\\."| grep -v _Z)
+bss_symbol_list=$(objdump -t "$LIBPROJ" | grep "g     O \\.bss" | awk '{print $6}' | grep -v "\\."| grep -v _Z)
 for symbol in $bss_symbol_list
 do
     echo "#define $symbol internal_$symbol" >> $OUT_FILE
